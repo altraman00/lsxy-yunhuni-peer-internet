b9b7284b40a49abe3e666428b24ee6634be41e44
liups <liups@yunhuni.com>
2016-06-21 15:46:41 +0800

    YHNI-81 基于RESTAPI安全授权方案  初步完成,修改successHandler

--- app-portal/src/main/java/com/lsxy/app/portal/rest/security/PortalAuthenticationSuccessHandler.java
+++ app-portal/src/main/java/com/lsxy/app/portal/rest/security/PortalAuthenticationSuccessHandler.java
@@ -1,7 +1,7 @@
 package com.lsxy.app.portal.rest.security;
 
 import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -12,7 +12,7 @@ import java.io.IOException;
 /**
  * Created by liups on 2016/6/21.
  */
-public class PortalAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+public class PortalAuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
     public static final String SSO_TOKEN = "X-Auth-Token";
 
     @Override
