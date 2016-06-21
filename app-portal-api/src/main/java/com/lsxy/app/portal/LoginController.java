68eb0ed36e358799f2b28efd91b0c0a12aad53da
谭畅 <tanchang@yunhuni.com>
2016-06-21 20:31:27 +0800

    YHNI-75 服务层REST RPC调用方案 完善安全处理方案 增加登陆测试用例

--- app-portal-api/src/main/java/com/lsxy/app/portal/LoginController.java
+++ app-portal-api/src/main/java/com/lsxy/app/portal/LoginController.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -21,7 +22,7 @@ public class LoginController {
     private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 
     @RequestMapping(path="/login",method = RequestMethod.POST)
-    public RestResponse login(){
+    public RestResponse login(@RequestParam String username,@RequestParam String password){
 
         UserRestToken token = new UserRestToken();
         return RestResponse.success(token);
--- app-portal-api/src/test/java/com/lsxy/app/portal/test/RestClientTest.java
+++ app-portal-api/src/test/java/com/lsxy/app/portal/test/RestClientTest.java
@@ -1,6 +1,8 @@
 package com.lsxy.app.portal.test;
 
 import com.lsxy.app.portal.MainClass;
+import com.lsxy.framework.web.rest.RestResponse;
+import org.apache.commons.collections.MapUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,10 +10,14 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.TestRestTemplate;
 import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.security.RunAs;
 import java.awt.print.Book;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Tandy on 2016/6/21.
@@ -21,16 +27,23 @@ import java.awt.print.Book;
 @WebIntegrationTest("server.port:0")
 public class RestClientTest {
 
-    @Value("${local.server.port}")
-    private int port;
+    @Value("http://localhost:${local.server.port}/")
+    private String restPrefixUrl;
 
 
     private RestTemplate restTemplate = new TestRestTemplate();
 
     @Test
     public void login() {
-        restTemplate.postForObject()
-        Book book = restTemplate.getForObject("http://localhost:" + port +"/login", Book.class);
+        String url = restPrefixUrl + "/login";
+        Map<String,String> formParams = new HashMap<>();
+        formParams.put("username","tanchang");
+        formParams.put("password","123");
+
+        MultiValueMap<String, String> requestEntity = new LinkedMultiValueMap<>();
+        formParams.keySet().stream().forEach(key -> requestEntity.add(key, MapUtils.getString(formParams, key, "")));
+        String restResponse = restTemplate.postForObject(url,requestEntity,String.class);
+        System.out.println(restResponse);
     }
 
 }
