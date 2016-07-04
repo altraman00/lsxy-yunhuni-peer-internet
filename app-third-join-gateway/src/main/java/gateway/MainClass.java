82cfe7dbfd5d76c810e958fe671cf3087a0a564b
谭畅 <tanchang@yunhuni.com>
2016-07-04 16:53:42 +0800

    为spring boot 应用程序提供可以定制端口号的方法

--- app-third-join-gateway/src/main/java/gateway/MainClass.java
+++ app-third-join-gateway/src/main/java/gateway/MainClass.java
@@ -3,11 +3,16 @@ package gateway;
 import com.lsxy.framework.FrameworkServiceConfig;
 import com.lsxy.framework.api.FrameworkApiConfig;
 import com.lsxy.framework.cache.FrameworkCacheConfig;
+import com.lsxy.framework.config.SystemConfig;
+import com.lsxy.framework.web.web.AbstractSpringBootStarter;
 import com.lsxy.yuhuni.api.YunhuniApiConfig;
 import com.lsxy.yunhuni.YunhuniServiceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.context.annotation.Import;
 
 /**
@@ -15,10 +20,15 @@ import org.springframework.context.annotation.Import;
  */
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @Import({FrameworkApiConfig.class,FrameworkServiceConfig.class, FrameworkCacheConfig.class, YunhuniApiConfig.class, YunhuniServiceConfig.class})
-public class MainClass {
+public class MainClass  extends AbstractSpringBootStarter{
 
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(MainClass.class, args);
+    }
+
+    @Override
+    public String systemId() {
+        return "third.join.gateway";
     }
 }
