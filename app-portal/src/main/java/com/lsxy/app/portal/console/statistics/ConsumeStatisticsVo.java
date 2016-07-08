f19815d90a4e53d8dbfa36650c583568fa724e3e
张锡斌 <zhangxb@yunhuni.com>
2016-07-08 12:00:17 +0800

    修改分页数据为异步分页

--- app-portal/src/main/java/com/lsxy/app/portal/console/statistics/ConsumeStatisticsVo.java
+++ app-portal/src/main/java/com/lsxy/app/portal/console/statistics/ConsumeStatisticsVo.java
@@ -9,13 +9,11 @@ import java.util.Date;
  * Created by zhangxb on 2016/7/6.
  */
 public class ConsumeStatisticsVo {
-    public static final String TYPE_MONTH = "month";//日统计类型 按月查找输出 返回按月
-    public static final String TYPE_YEAR = "year";//月统计类型 按年查找输出 返回按年
-    private String type = TYPE_MONTH;//类型统计类型 month year
+    public static final String TYPE_MONTH = "month";//月统计类型 按年查找输出 返回按年
+    public static final String TYPE_DAY = "day";//日统计类型 按月查找输出 返回按月
+    private String type = TYPE_MONTH;//类型统计类型 month day
     private String startTime = DateUtils.formatDate(new Date(),"yyyy-MM");//时间
     private String endTime = "";//比较时间
-    private String startTimeYear = DateUtils.formatDate(new Date(),"yyyy");//时间
-    private String endTimeYear = "";//比较时间
     private String appId = "0";//应用编号
 
     public ConsumeStatisticsVo() {
@@ -57,21 +55,5 @@ public class ConsumeStatisticsVo {
 
     public void setAppId(String appId) {
         this.appId = appId;
-    }
-
-    public String getStartTimeYear() {
-        return startTimeYear;
-    }
-
-    public void setStartTimeYear(String startTimeYear) {
-        this.startTimeYear = startTimeYear;
-    }
-
-    public String getEndTimeYear() {
-        return endTimeYear;
-    }
-
-    public void setEndTimeYear(String endTimeYear) {
-        this.endTimeYear = endTimeYear;
     }
 }
