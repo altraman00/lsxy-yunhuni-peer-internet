35a9e1b78290efc2683f439e84542c9ebc1d7703
张锡斌 <zhangxb@yunhuni.com>
2016-07-18 14:09:31 +0800

    修改细节1

--- app-portal-api/src/main/java/com/lsxy/app/portal/base/DateConverter.java
+++ app-portal-api/src/main/java/com/lsxy/app/portal/base/DateConverter.java
@@ -1,15 +1,11 @@
 package com.lsxy.app.portal.base;
-
 import com.lsxy.framework.core.utils.DateUtils;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
-
-import java.text.SimpleDateFormat;
 import java.util.Date;
-
 /**
  * 增加处理时间戳转换成日期对象配置
  * Created by zhangxb on 2016/7/13.
@@ -19,10 +15,9 @@ public class DateConverter implements Converter<String, Date> {
     private static final Logger logger = LoggerFactory.getLogger(DateConverter.class);
     @Override
     public Date convert(String s) {
-        if(s==null||s.length()==0){return null;}
         long ltime = 0L;
         Date date = null;
-        if(StringUtils.isNotBlank(s)){
+        if(StringUtils.isNotEmpty(s)){
             try{
                 ltime = Long.parseLong(s);
                 date = new Date(ltime);
