1df3aa20c02fe25e9f5bab50c60a4c38e488dfa8
谭畅 <tanchang@yunhuni.com>
2016-07-19 14:25:03 +0800

    area-server spring boot frmework start

--- app-area-server/src/main/java/com/lsxy/area/server/MainClass.java
+++ app-area-server/src/main/java/com/lsxy/area/server/MainClass.java
@@ -1,11 +1,15 @@
+package com.lsxy.area.server;
+
 import com.lsxy.framework.web.web.AbstractSpringBootStarter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Created by tandy on 16/7/19.
  */
 @SpringBootApplication
+@ComponentScan
 public class MainClass extends AbstractSpringBootStarter{
 
     @Override
