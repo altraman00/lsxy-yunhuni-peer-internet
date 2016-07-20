1d1e1f84c77ef84e1664c2e8f2aab95502f636ee
谭畅 <tanchang@yunhuni.com>
2016-07-20 16:04:45 +0800

    netty server

--- app-area-server/src/main/java/com/lsxy/area/server/AreaServer.java
+++ app-area-server/src/main/java/com/lsxy/area/server/AreaServer.java
@@ -1,78 +1,54 @@
 package com.lsxy.area.server;
 
-import org.jboss.netty.bootstrap.ServerBootstrap;
-import org.jboss.netty.channel.*;
-import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.net.InetSocketAddress;
-import java.util.concurrent.Executors;
 
 /**
  * Createdb by tandy on 16/7/19.
- *
  */
 @Component
-public class   AreaServer {
+public class AreaServer {
 
     private static final Logger logger = LoggerFactory.getLogger(AreaServer.class);
 
 
     @PostConstruct
-    public void start(){
+    public void start() throws InterruptedException {
 
-        if(logger.isDebugEnabled()){
+        if (logger.isDebugEnabled()) {
             logger.debug("开始启动区域管理服务器....");
         }
-        ServerBootstrap bootstrap = new ServerBootstrap(new NioServerSocketChannelFactory(Executors.newCachedThreadPool(),Executors.newCachedThreadPool()));
-        bootstrap.setPipelineFactory(new ChannelPipelineFactory() {
-            @Override
-            public ChannelPipeline getPipeline() throws Exception {
-                return Channels.pipeline(new HelloServerHandler());
-            }
-        });
-
-
         int port = 8888;
-        bootstrap.bind(new InetSocketAddress(port));
-        if(logger.isDebugEnabled()){
-            logger.debug("区域管理服务器启动成功,绑定端口:{}",port);
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup);
+            b.channel(NioServerSocketChannel.class);
+            b.childHandler(new HelloServerInitializer());
+
+            // 服务器绑定端口监听
+            ChannelFuture f = b.bind(port).sync();
+            // 监听服务器关闭监听
+            f.channel().closeFuture().sync();
+            // 可以简写为
+            /* b.bind(portNumber).sync().channel().closeFuture().sync(); */
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+        if (logger.isDebugEnabled()) {
+            logger.debug("区域管理服务器启动成功,绑定端口:{}", port);
         }
 
     }
-
-    private static class HelloServerHandler extends SimpleChannelHandler {
-
-
-
-        /**
-
-         * 当有客户端绑定到服务端的时候触发，打印"Hello world, I'm server."
-
-         *
-
-         * @alia OneCoder
-
-         * @author lihzh
-
-         */
-
-        @Override
-
-        public void channelConnected(ChannelHandlerContext ctx,
-
-                                     ChannelStateEvent e) {
-            if(logger.isDebugEnabled()){
-                logger.debug("有客户端连接经来了");
-            }
-            System.out.println("Hello world, I'm server.");
-
-        }
-
-    }
-
-
 }
