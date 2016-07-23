554dfc6f1792eeade2b858e9559ef6f35d04a71b
张锡斌 <zhangxb@yunhuni.com>
2016-07-23 16:52:24 +0800

    修改细节

--- app-portal-api/src/main/java/com/lsxy/app/portal/rest/stastistic/VoiceCdrController.java
+++ app-portal-api/src/main/java/com/lsxy/app/portal/rest/stastistic/VoiceCdrController.java
@@ -20,11 +20,29 @@ import java.math.BigDecimal;
 public class VoiceCdrController extends AbstractRestController {
     @Autowired
     VoiceCdrService voiceCdrService;
+
+    /**
+     * 获取分页数据
+     * @param pageNo 当前页
+     * @param pageSize 每页记录数
+     * @param type 类型
+     * @param time 时间
+     * @param appId 应用id
+     * @return
+     */
     @RequestMapping("/plist")
     public RestResponse pageList(Integer pageNo,Integer pageSize,Integer type,String time,String appId){
         Page<VoiceCdr> page = voiceCdrService.pageList(pageNo,pageSize,type,getCurrentAccount().getTenant().getId(),time,appId);
         return RestResponse.success(page);
     }
+
+    /**
+     * 统计租户消费情况
+     * @param type 类型
+     * @param time 时间
+     * @param appId 应用id
+     * @return
+     */
     @RequestMapping("/sum")
     public RestResponse sumCdr(Integer type,String time,String appId){
         BigDecimal sum = voiceCdrService.sumCost(type,getCurrentAccount().getTenant().getId(),time,appId);
