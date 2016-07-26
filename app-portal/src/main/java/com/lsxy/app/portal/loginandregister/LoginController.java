932f8221d3e666479da04e3633b6379294055a98
张锡斌 <zhangxb@yunhuni.com>
2016-07-26 15:50:13 +0800

    规范ajax请求处理，及超时处理

--- app-portal/src/main/java/com/lsxy/app/portal/loginandregister/LoginController.java
+++ app-portal/src/main/java/com/lsxy/app/portal/loginandregister/LoginController.java
@@ -1,8 +1,14 @@
 package com.lsxy.app.portal.loginandregister;
 
+import com.lsxy.app.portal.comm.MapBean;
+import com.lsxy.framework.core.utils.JSONUtil;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * 登录处理器
@@ -11,8 +17,22 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class LoginController {
     @RequestMapping("/login")
-    public ModelAndView login(){
-        String toUrl = "login";
-        return new ModelAndView(toUrl);
+    public ModelAndView login(HttpServletRequest request,HttpServletResponse response){
+        String xRequestedWith = request.getHeader("X-Requested-With");
+        //处理ajax异常的时候返回登陆页面
+        if (StringUtils.isNotBlank(xRequestedWith) && "XMLHttpRequest".equals(xRequestedWith)) {
+            MapBean map=new MapBean();
+            map.setCode("0010");
+            map.setMsg("登录超时");
+            map.setData("/login");
+            String result = JSONUtil.objectToJson(map);
+            try {
+                response.getWriter().write(result);
+            }catch (Exception e){}
+            return null;
+        }else {
+            String toUrl = "login";
+            return new ModelAndView(toUrl);
+        }
     }
 }
