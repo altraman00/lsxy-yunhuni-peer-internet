f69a0a28d154065562006fbf0f3f487176d9bc5f
张锡斌 <zhangxb@yunhuni.com>
2016-07-27 11:56:41 +0800

    TT53呼入号码管理内容列表显示问题

--- module-service/src/main/java/com/lsxy/yunhuni/resourceTelenum/service/ResourcesRentServiceImpl.java
+++ module-service/src/main/java/com/lsxy/yunhuni/resourceTelenum/service/ResourcesRentServiceImpl.java
@@ -36,7 +36,7 @@ public class ResourcesRentServiceImpl extends AbstractService<ResourcesRent> imp
     public Page<ResourcesRent> pageListByTenantId(String userName,int pageNo, int pageSize) throws MatchMutiEntitiesException {
         Tenant tenant = tenantService.findTenantByUserName(userName);
         //where obj.tenant.id=?1 -->这写法报错。暂时不知道原因，先手动拼装SQL
-        String hql = "from ResourcesRent obj where obj.tenant.id=?1";
+        String hql = "from ResourcesRent obj where obj.tenant.id=?1 and obj.rentExpire<>3 ";
         Page<ResourcesRent> page =  this.pageList(hql,pageNo,pageSize,tenant.getId());
         return page;
     }
