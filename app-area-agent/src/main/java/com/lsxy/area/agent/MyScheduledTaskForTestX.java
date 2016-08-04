7c3e1be845409f4218cebe97ac9c64f766c15aff
tanchang <password>
2016-08-04 17:06:31 +0800

    修改客户端测试方案

--- app-area-agent/src/main/java/com/lsxy/area/agent/MyScheduledTaskForTestX.java
+++ app-area-agent/src/main/java/com/lsxy/area/agent/MyScheduledTaskForTestX.java
@@ -10,6 +10,9 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by tandy on 16/7/30.
  */
@@ -24,13 +27,39 @@ public class MyScheduledTaskForTestX {
     private Client client;
 
     private static final Logger logger = LoggerFactory.getLogger(MyScheduledTaskForTestX.class);
-    @Scheduled(fixedDelay=1)
-    public void doTest(){
+
+
+//    @Scheduled(fixedDelay=1)
+//    public void doTest(){
+//        if(logger.isDebugEnabled()){
+//        }
+//        Session session = client.getAvalibleSession();
+//        if(session != null){
+//            RPCRequest request = RPCRequest.newRequest("REQUEST","VALUE0=001");
+//            session.write(request);
+//        }
+//    }
+
+    @PostConstruct
+    public void doTest() throws InterruptedException {
         if(logger.isDebugEnabled()){
+            logger.debug("开始测试");
         }
-        Session session = client.getAvalibleSession();
-        if(session != null){
-            RPCRequest request = RPCRequest.newRequest("REQUEST","VALUE0=001");
+        Session session = null;
+        while(true){
+            session = client.getAvalibleSession();
+            if(session == null || !session.isValid()){
+                if(logger.isDebugEnabled()){
+                    logger.debug("没有有效的session,等着吧");
+                }
+                TimeUnit.SECONDS.sleep(1);
+            }else{
+                break;
+            }
+        }
+
+        RPCRequest request = RPCRequest.newRequest("HELLO","param01=001");
+        while(true){
             session.write(request);
         }
     }
