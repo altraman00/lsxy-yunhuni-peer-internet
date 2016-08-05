1f04fab7d095c48917725697340c4925201b0b11
tanchang <password>
2016-08-05 18:06:12 +0800

    修改开发环境配置

--- app-area-agent/src/main/java/com/lsxy/area/agent/cti/CTIClient.java
+++ app-area-agent/src/main/java/com/lsxy/area/agent/cti/CTIClient.java
@@ -19,7 +19,7 @@ import java.util.Set;
  * CTI 客户端启动器  需要配合JNI使用
  */
 @Component
-@Profile(value={"test","production"})
+@Profile(value={"test","production","development"})
 public class CTIClient implements RpcEventListener{
 
 
