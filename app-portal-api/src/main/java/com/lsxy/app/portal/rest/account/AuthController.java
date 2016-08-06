b64dbe9858ba2a4c565260ade04152d37c1c70ff
张锡斌 <zhangxb@yunhuni.com>
2016-08-06 15:23:57 +0800

    修改6

--- app-portal-api/src/main/java/com/lsxy/app/portal/rest/account/AuthController.java
+++ app-portal-api/src/main/java/com/lsxy/app/portal/rest/account/AuthController.java
@@ -116,7 +116,12 @@ public class AuthController extends AbstractRestController {
         //获取租户对象
         Account acount  = accountService.findAccountByUserName(userName);
         Tenant tenant = acount.getTenant();
-        tenant.setIsRealAuth(Integer.valueOf(status));
+        Integer statusT = Integer.valueOf(status);
+        tenant.setIsRealAuth(statusT);
+        if(statusT==Tenant.AUTH_ONESELF_WAIT){
+            statusT=Tenant.AUTH_WAIT;
+        }
+        realnamePrivate.setStatus(statusT);
         realnamePrivate.setTenant(tenant);
         //保存到数据库
         realnamePrivate = realnaePrivateService.save(realnamePrivate);
