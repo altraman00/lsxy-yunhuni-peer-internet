f03e40ae3be4e7469644335ee13bb70b19218f90
张锡斌 <zhangxb@yunhuni.com>
2016-08-15 15:07:03 +0800

    修改1

--- module-service/src/main/java/com/lsxy/framework/message/service/MessageServiceImpl.java
+++ module-service/src/main/java/com/lsxy/framework/message/service/MessageServiceImpl.java
@@ -30,13 +30,57 @@ public class MessageServiceImpl extends AbstractService<Message> implements Mess
 
     @Override
     public Page<Message> pageList(String type, String startTime, String endTime, Integer pageNo, Integer pageSize) {
-        Date date1 = DateUtils.parseDate(startTime,"yyyy-MM-dd");
-        Date date2 = DateUtils.parseDate(endTime+" 23:59:59","yyyy-MM-dd HH:mm:ss");
-        String hql = "from Message obj where obj.lineTime>=?1 and obj.lineTime<=?2 ";
-        if(StringUtil.isNotEmpty(type)){
-            hql+=" and obj.type='"+type+"' ";
+
+        Date date1 = null;
+        Date date2 = null;
+        try {
+            date1 = DateUtils.parseDate(startTime, "yyyy-MM-dd");
+        }catch (Exception e){}
+        try {
+            date2 = DateUtils.parseDate(endTime + " 23:59:59", "yyyy-MM-dd HH:mm:ss");
+        }catch (Exception e){}
+        String hql = "from Message obj ";
+        boolean flag = true;
+        int i=1;
+        if(date1!=null){
+            if(flag){
+                hql +=" where ";
+                flag=false;
+            }else{
+                hql +=" and ";
+            }
+            hql +=" obj.lineTime>=?"+i;
+            i++;
         }
-        Page<Message> page =  this.pageList(hql,pageNo,pageSize,date1,date2);
+        if(date2!=null){
+            if(flag){
+                hql +=" where ";
+                flag=false;
+            }else{
+                hql +=" and ";
+            }
+            hql +=" obj.lineTime<=?"+i;
+            i++;
+        }
+        if(StringUtil.isNotEmpty(type)){
+            if(flag){
+                hql +=" where ";
+                flag=false;
+            }else{
+                hql +=" and ";
+            }
+            hql+=" obj.type='"+type+"' ";
+        }
+        Page<Message> page = null;
+        if(date1!=null&&date2!=null){
+            page = this.pageList(hql,pageNo,pageSize,date1,date2);
+        }else if(date1!=null&&date2==null){
+            page = this.pageList(hql,pageNo,pageSize,date1);
+        }else if(date1==null&&date2!=null){
+            page = this.pageList(hql,pageNo,pageSize,date2);
+        }else{
+            page = this.pageList(hql,pageNo,pageSize);
+        }
         return page;
     }
 }
