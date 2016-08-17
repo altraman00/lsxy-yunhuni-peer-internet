ebceebba8d5515b949b1885a0419a6e6bdbfab26
tanchang <password>
2016-08-17 18:58:46 +0800

    修改测试数据

--- app-api-gateway/src/main/java/com/lsxy/app/api/gateway/rest/TestController.java
+++ app-api-gateway/src/main/java/com/lsxy/app/api/gateway/rest/TestController.java
@@ -3,6 +3,7 @@ package com.lsxy.app.api.gateway.rest;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.lsxy.app.api.gateway.StasticsCounter;
 import com.lsxy.framework.api.test.TestService;
+import com.lsxy.framework.core.utils.DateUtils;
 import com.lsxy.framework.core.utils.UUIDGenerator;
 import com.lsxy.framework.mq.MQStasticCounter;
 import com.lsxy.framework.mq.api.MQService;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -139,8 +141,8 @@ public class TestController {
     }
 
 //    public static void main(String[] args) {
-//        Date stdt = DateUtils.parseDate("2016-08-18 14:27:26.078","yyyy-MM-dd HH:mm:ss.SSS");
-//        Date enddt = DateUtils.parseDate("2016-08-18 14:39:06.349","yyyy-MM-dd HH:mm:ss.SSS");
+//        Date stdt = DateUtils.parseDate("2016-08-17 17:49:14.641","yyyy-MM-dd HH:mm:ss.SSS");
+//        Date enddt = DateUtils.parseDate("2016-08-17 18:04:26.602","yyyy-MM-dd HH:mm:ss.SSS");
 //        System.out.println(enddt.getTime() - stdt.getTime() );
 //    }
 
