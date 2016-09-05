b616892fdeb7dac7a835d3926ca73efc6ad98757
liups <liups@yunhuni.com>
2016-09-05 11:36:34 +0800

    TT-136  修正类引用

--- module-service/src/main/java/com/lsxy/yunhuni/billing/service/BillingServiceImpl.java
+++ module-service/src/main/java/com/lsxy/yunhuni/billing/service/BillingServiceImpl.java
@@ -4,13 +4,10 @@ import com.lsxy.framework.api.base.BaseDaoInterface;
 import com.lsxy.framework.api.tenant.model.Tenant;
 import com.lsxy.framework.api.tenant.service.TenantService;
 import com.lsxy.framework.base.AbstractService;
-import com.lsxy.framework.cache.manager.RedisCacheService;
 import com.lsxy.framework.core.exceptions.MatchMutiEntitiesException;
-import com.lsxy.framework.core.utils.DateUtils;
 import com.lsxy.yunhuni.api.billing.model.Billing;
 import com.lsxy.yunhuni.api.billing.service.BillingService;
 import com.lsxy.yunhuni.billing.dao.BillingDao;
-import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
@@ -19,12 +16,6 @@ import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 
 import java.io.Serializable;
-import java.math.BigDecimal;
-import java.util.Date;
-
-import static com.lsxy.yunhuni.api.billing.service.CalBillingService.AMOUNT_REDIS_MULTIPLE;
-import static com.lsxy.yunhuni.api.billing.service.CalBillingService.USE_BALANCE_PREFIX;
-import static javafx.scene.input.KeyCode.T;
 
 /**
  * Created by liups on 2016/6/28.
