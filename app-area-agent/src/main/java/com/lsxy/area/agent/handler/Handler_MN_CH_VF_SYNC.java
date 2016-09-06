a3acb3539596d03c74d43c884115eb89860d58d1
张锡斌 <zhangxb@yunhuni.com>
2016-09-06 09:35:43 +0800

    同步代码

--- app-area-agent/src/main/java/com/lsxy/area/agent/handler/Handler_MN_CH_VF_SYNC.java
+++ app-area-agent/src/main/java/com/lsxy/area/agent/handler/Handler_MN_CH_VF_SYNC.java
@@ -1,5 +1,6 @@
 package com.lsxy.area.agent.handler;
 
+
 import com.alibaba.fastjson.JSON;
 import com.lsxy.framework.config.SystemConfig;
 import com.lsxy.framework.core.utils.DateUtils;
@@ -11,7 +12,6 @@ import com.lsxy.framework.rpc.api.RPCResponse;
 import com.lsxy.framework.rpc.api.ServiceConstants;
 import com.lsxy.framework.rpc.api.handler.RpcRequestHandler;
 import com.lsxy.framework.rpc.api.session.Session;
-import com.lsxy.yunhuni.api.file.model.VoiceFilePlay;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -73,14 +73,14 @@ public class Handler_MN_CH_VF_SYNC extends RpcRequestHandler{
      * @return
      */
     private Integer downFile(String uri,String path){
-        Integer result = VoiceFilePlay.SYNC_FAIL;
+        Integer result = -1;
         logger.error("文件下载开始{}---->{},时间:{}",uri,path, DateUtils.formatDate(new Date(),"yyyy-MM-dd HH:mm:ss"));
         File newFile = new File(path);
         long start = new Date().getTime();
         //先判断文件是否存在，如果存在则不下载
         if(newFile.exists()){
             logger.info("文件已存在不用下载，文件:{},下载信息:{}",path,uri);
-            result = VoiceFilePlay.SYNC_SUCCESS;
+            result = 1;
         }else{
             //补充文件夹
             new File(path.substring(0,path.lastIndexOf("/"))).mkdirs();
@@ -95,7 +95,7 @@ public class Handler_MN_CH_VF_SYNC extends RpcRequestHandler{
                 while ((length = in.read(buffer)) > 0) {
                     out.write(buffer, 0, length);
                 }
-                result = VoiceFilePlay.SYNC_SUCCESS;
+                result = 1;
             } catch (Exception e) {
                 logger.error("文件流输出异常,{]", e);
             }finally {
