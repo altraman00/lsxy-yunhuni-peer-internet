9ec274af24c39c790f9b33330ba61761ff950cd0
liups <liups@yunhuni.com>
2016-09-07 14:37:16 +0800

    YHNI-129  号码红黑名单过滤

--- module-service/src/main/java/com/lsxy/yunhuni/session/service/NotifyCallServiceImpl.java
+++ module-service/src/main/java/com/lsxy/yunhuni/session/service/NotifyCallServiceImpl.java
@@ -6,12 +6,14 @@ import com.lsxy.yunhuni.api.session.model.NotifyCall;
 import com.lsxy.yunhuni.api.session.service.NotifyCallService;
 import com.lsxy.yunhuni.session.dao.NotifyCallDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.io.Serializable;
 
 /**
  * Created by liups on 2016/9/6.
  */
+@Service
 public class NotifyCallServiceImpl extends AbstractService<NotifyCall> implements NotifyCallService {
     @Autowired
     NotifyCallDao notifyCallDao;
