e621c39c7e1873c1e67066dead207f908a2f8778
tanchang <tanchang@yunhuni.com>
2016-09-08 15:52:12 +0800

    modify test config

--- app-area-agent/src/main/java/com/lsxy/area/agent/AreaClient.java
+++ app-area-agent/src/main/java/com/lsxy/area/agent/AreaClient.java
@@ -25,7 +25,8 @@ public class AreaClient {
     @Value("${area.agent.server.url}")
     private String serverUrl;
 
-    @Value("${area.agent.areaid}")
+    //通过环境变量配置该参数,不支持".",只支持"_"
+    @Value("${area_agent_areaid:area001}")
     private String areaid;
 
     //由于需要通过设置环境变量设置该值,需要适用下划线,不支持"."
