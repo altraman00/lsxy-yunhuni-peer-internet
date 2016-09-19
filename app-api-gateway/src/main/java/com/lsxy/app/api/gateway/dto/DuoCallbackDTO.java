781291128832e656d919ecc9eb3ef5d8b51a2bc9
liups <liups@yunhuni.com>
2016-09-19 13:58:59 +0800

    调整语音回拔Api调用代码结构

--- app-api-gateway/src/main/java/com/lsxy/app/api/gateway/dto/DuoCallbackDTO.java
+++ app-api-gateway/src/main/java/com/lsxy/app/api/gateway/dto/DuoCallbackDTO.java
@@ -1,5 +1,9 @@
-package com.lsxy.area.api;
+package com.lsxy.app.api.gateway.dto;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 /**
@@ -8,19 +12,32 @@ import java.io.Serializable;
  */
 public class DuoCallbackDTO implements Serializable {
     private String from1;               //第一方主叫号码
+    @NotNull
     private String to1;                 //第一方被叫号码
     private String from2;               //第二方主叫号码
+    @NotNull
     private String to2;                 //第二方被叫号码
     private String ring_tone;           //自定义回铃音
+    @Min(0)
+    @Max(2)
     private Integer ring_tone_mode = 0;     //自定义回铃音播放模式0：收到对端回铃后开始播放 1：拨号时即开始播放，收到对端回铃后停止播放 2：拨号时即开始播放，对端接听或者挂机后停止播放
+    @NotNull
+    @Min(1)
+    @Max(5 * 60)
     private Integer max_dial_duration;  //最大拨号等待时间（秒）
+    @NotNull
+    @Min(1)
+    @Max(60 * 60 * 6)
     private Integer max_call_duration;  // 最大接通时间（秒）
     private Boolean recording = false ;         //是否录音
+    @Min(0)
+    @Max(2)
     private Integer record_mode = 0;        // 录音模式0: 双向接通后录音  1：开始呼叫第一方时启动录音  2: 开始呼叫第二方时启动录音
 //    private Integer countdown_time ;       //倒计时时间点
 //    private String countdown_voice;       //倒计时播放语音文件
 //    private String callback_url;        //结果通知 HTTP 地址
 //    private String callback_method = "POST";     //结果通知 HTTP 方法, GET(默认) or POST
+    @Size(max = 128)
     private String user_data;           //用户数据
 
     public String getFrom1() {
