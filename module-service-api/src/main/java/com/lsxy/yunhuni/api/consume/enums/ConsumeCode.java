6e1d8abf1495efbb0fe134a3ffb52d9abc977356
liups <liups@yunhuni.com>
2016-09-20 20:02:59 +0800

    TT-452 消费类型转换为中文

---
+++ module-service-api/src/main/java/com/lsxy/yunhuni/api/consume/enums/ConsumeCode.java
@@ -0,0 +1,23 @@
+package com.lsxy.yunhuni.api.consume.enums;
+
+/**
+ * 消费类型code，一部份与产品一致
+ * Created by liups on 2016/9/20.
+ */
+public enum ConsumeCode {
+    duo_call("语音回拔"),
+    sys_conf("语音会议"),
+    ivr_call("语音IVR"),
+    captcha_call("语音验证码"),
+    notify_call("语音通知"),
+    rent_number("号码租用");
+
+    private String name;
+    ConsumeCode(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
