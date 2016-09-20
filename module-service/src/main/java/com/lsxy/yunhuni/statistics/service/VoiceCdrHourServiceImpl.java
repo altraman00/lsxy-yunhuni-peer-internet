f5602044ded9d9580ba5276c7ae6b0d390142b37
liups <liups@yunhuni.com>
2016-09-20 16:38:25 +0800

    api统计字段修改

--- module-service/src/main/java/com/lsxy/yunhuni/statistics/service/VoiceCdrHourServiceImpl.java
+++ module-service/src/main/java/com/lsxy/yunhuni/statistics/service/VoiceCdrHourServiceImpl.java
@@ -42,7 +42,7 @@ public class VoiceCdrHourServiceImpl extends AbstractService<VoiceCdrHour> imple
         String selects = map.get("selects");
         String groupbys = map.get("groupbys");
         String wheres = map.get("wheres");
-        String sql =" insert into db_lsxy_bi_yunhuni.tb_bi_voice_cdr_hour("+selects+" id,dt,hour,among_duration,among_connect,among_not_connect,among_call,create_time,last_time,deleted,sortno,version )" +
+        String sql =" insert into db_lsxy_bi_yunhuni.tb_bi_voice_cdr_hour("+selects+" id,dt,hour,among_cost_time,among_duration,among_connect,among_not_connect,among_call,create_time,last_time,deleted,sortno,version )" +
                 " select "+selects+"  REPLACE(UUID(), '-', '') as id, ? as dt,? as day, "+
                 " IFNULL(sum(cost_time_long),0) as among_cost_time," +
                 " IFNULL(sum(call_time_long),0) as among_duration," +
