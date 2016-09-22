8d3b2484860413e7cb23f9a1cf43c96f19edb90a
tanchang <tanchang@yunhuni.com>
2016-09-22 13:30:11 +0800

    fix bug

--- app-oc/src/test/java/com/lsxy/app/oc/test/ConsumerTest.java
+++ app-oc/src/test/java/com/lsxy/app/oc/test/ConsumerTest.java
@@ -39,8 +39,8 @@ public class ConsumerTest {
     @Test
     public void testSendMsg(){
         String token = login();
-        String tenantid="8a2a6a4a57282a21015735ef3d7b0003";
-        String url = restPrefixUrl + "/tenant/tenants/"+tenantid+"/consumes?year=2016&month=7&pageNo=1&pageSize=1";
+        String tenantid="8a2bc67257282a250157282b75740000";
+        String url = restPrefixUrl + "/tenant/tenants/"+tenantid+"/consumes?year=2016&month=9&pageNo=1&pageSize=10";
 
 //        @RequestParam Integer year,
 //        @RequestParam Integer month,
