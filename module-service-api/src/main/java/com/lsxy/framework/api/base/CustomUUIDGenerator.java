9d148c8868ced94304b89094ea133af053b8a607
liuws@yunhuni.com <liuws@yunhuni.com>
2016-09-23 18:51:22 +0800

    test

--- module-service-api/src/main/java/com/lsxy/framework/api/base/CustomUUIDGenerator.java
+++ module-service-api/src/main/java/com/lsxy/framework/api/base/CustomUUIDGenerator.java
@@ -1,14 +1,14 @@
 package com.lsxy.framework.api.base;
 
 import org.hibernate.engine.spi.SessionImplementor;
-import org.hibernate.id.UUIDGenerator;
+import org.hibernate.id.UUIDHexGenerator;
 
 import java.io.Serializable;
 
 /**
  * Created by liups on 2016/9/23.
  */
-public class CustomUUIDGenerator extends UUIDGenerator {
+public class CustomUUIDGenerator extends UUIDHexGenerator {
     public Serializable generate(SessionImplementor session, Object obj) {
         if (obj instanceof IdEntity
                 && (((IdEntity) obj).getId() != null
