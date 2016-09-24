5a7fc48584e986766da1f9db14546ac1cf284bc6
liups <liups@yunhuni.com>
2016-09-24 12:16:15 +0800

    YHNI-162 修正语音通知映射类

--- app-area-server/src/main/java/com/lsxy/area/server/event/handler/Handler_EVENT_SYS_ON_CHAN_CLOSED.java
+++ app-area-server/src/main/java/com/lsxy/area/server/event/handler/Handler_EVENT_SYS_ON_CHAN_CLOSED.java
@@ -62,6 +62,7 @@ public class Handler_EVENT_SYS_ON_CHAN_CLOSED extends EventHandler{
         if(businessState == null){
             voiceCdrService.save(voiceCdr);
             logger.error("返回CDR找不到关联的业务数据,cdr.id：{}",voiceCdr.getId());
+            return null;
         }
         voiceCdr.setAreaId(businessState.getAreaId());
         voiceCdr.setTenantId(businessState.getTenantId());
