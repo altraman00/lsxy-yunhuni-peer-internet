e1cf34699e80c802adb9ba97f250043f3d5d788d
liuws@yunhuni.com <liuws@yunhuni.com>
2016-10-14 14:37:49 +0800

    调用cti的回调不返回user_data

--- app-area-agent/src/main/java/com/lsxy/area/agent/handler/simplecall/Handler_MN_CH_EXT_DUO_CALLBACK_CANCEL.java
+++ app-area-agent/src/main/java/com/lsxy/area/agent/handler/simplecall/Handler_MN_CH_EXT_DUO_CALLBACK_CANCEL.java
@@ -60,7 +60,7 @@ public class Handler_MN_CH_EXT_DUO_CALLBACK_CANCEL extends RpcRequestHandler{
         }
 
         Map<String, Object> params = request.getParamMap();
-
+        String call_id = (String)params.get("user_data");
         try {
             if(logger.isDebugEnabled()){
                 logger.debug("调用CTI取消双向回拔，参数为{}", JSONUtil.objectToJson(params));
@@ -70,17 +70,17 @@ public class Handler_MN_CH_EXT_DUO_CALLBACK_CANCEL extends RpcRequestHandler{
                 @Override
                 protected void onResult(Object o) {
                     if(logger.isDebugEnabled()){
-                        logger.debug("调用ext.duo_callback.cancel成功，call_id={},result={}",params.get("user_data"),o);
+                        logger.debug("调用ext.duo_callback.cancel成功，call_id={},result={}",call_id,o);
                     }
                 }
 
                 @Override
                 protected void onError(RpcError rpcError) {
-                    logger.error("调用ext.duo_callback失败call_id={},result={}",params.get("user_data"),rpcError);
+                    logger.error("调用ext.duo_callback失败call_id={},result={}",call_id,rpcError);
                     RPCRequest req = RPCRequest.newRequest(ServiceConstants.CH_MN_CTI_EVENT,
                             new MapBuilder<String,Object>()
                                     .put("method",Constants.EVENT_EXT_CALL_ON_FAIL)
-                                    .put("user_data",params.get("user_data"))
+                                    .put("user_data",call_id)
                                     .build());
                     try {
                         rpcCaller.invoke(sessionContext,req);
@@ -91,11 +91,11 @@ public class Handler_MN_CH_EXT_DUO_CALLBACK_CANCEL extends RpcRequestHandler{
 
                 @Override
                 protected void onTimeout() {
-                    logger.error("调用ext.duo_callback超时call_id={}",params.get("user_data"));
+                    logger.error("调用ext.duo_callback超时call_id={}",call_id);
                     RPCRequest req = RPCRequest.newRequest(ServiceConstants.CH_MN_CTI_EVENT,
                             new MapBuilder<String,Object>()
                                     .put("method",Constants.EVENT_SYS_CALL_ON_TIMEOUT)
-                                    .put("user_data",params.get("user_data"))
+                                    .put("user_data",call_id)
                                     .build());
                     try {
                         rpcCaller.invoke(sessionContext,req);
