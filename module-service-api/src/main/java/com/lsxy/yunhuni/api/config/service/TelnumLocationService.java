4a0ca6f5d589c25c27d7c9b9e8a8dec88a1f7008
zhangxb <295235027@qq.com>
2016-10-26 16:06:49 +0800

    Revert "修改1"
    This reverts commit c4b7fda8a2e3f3d0007d5fc24076339c592b57c6.

---
+++ module-service-api/src/main/java/com/lsxy/yunhuni/api/config/service/TelnumLocationService.java
@@ -0,0 +1,32 @@
+package com.lsxy.yunhuni.api.config.service;
+
+import com.lsxy.framework.api.base.BaseService;
+import com.lsxy.yunhuni.api.config.model.TelnumLocation;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by zhangxb on 2016/10/25.
+ */
+public interface TelnumLocationService extends BaseService<TelnumLocation> {
+    /**
+     * 获取全部省份
+     * @return
+     */
+    List<String> getProvinceList();
+
+    /**
+     * 根据省份获取城市和区号
+     * @param province
+     * @return
+     */
+    List<Map<String,Object>> getCityAndAreaCode(String province );
+
+    /**
+     * 获取区号
+     * @param telphone
+     * @return
+     */
+    String getAreaCodeOfTelephone(String telphone);
+}
