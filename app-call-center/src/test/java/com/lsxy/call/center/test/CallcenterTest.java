65ccd8f515d7480c28c3052175916f9b965d412a
liuws@yunhuni.com <liuws@yunhuni.com>
2016-11-01 19:10:49 +0800

    enqueue 表达式 解析成sql

--- app-call-center/src/test/java/com/lsxy/call/center/test/CallcenterTest.java
+++ app-call-center/src/test/java/com/lsxy/call/center/test/CallcenterTest.java
@@ -14,7 +14,13 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 /**
  * Created by Liuws on 2016/7/14.
@@ -45,33 +51,49 @@ public class CallcenterTest {
         appExtension.setEnabled(1);
         appExtension.setTenantId("40288ac9575612a30157561c7ff50004");
         appExtension.setAppId("40288ac957e1812e0157e18a994e0000");
-        appExtension.setName("123456");
+        appExtension.setName("1234561");
         appExtension.setType(AppExtension.TYPE_SIP);
-        appExtension.setUser("123456");
-        appExtension.setPassword("123456");
+        appExtension.setUser("1234561");
+        appExtension.setPassword("1234561");
         appExtension.setSecret("");
         appExtension.setRegistrar("");
-        appExtension.setRegisterExpires(180);
-        appExtension.setLastRegisterTime(null);
-        appExtension.setLastRegisterStatus(null);
+        appExtension.setRegisterExpires(18000);
+        appExtension.setLastRegisterTime(new Date());
+        appExtension.setLastRegisterStatus(200);
         appExtensionService.register(appExtension);
     }
 
     @Test
-    public void test3(){
-        CallCenterAgent callCenterAgent = new CallCenterAgent();
-        callCenterAgent.setName("test1");
-        callCenterAgent.setExtentions(appExtensionService.pageList(1,10).getResult());
-
-        List<AgentSkill> skills = new ArrayList<>();
-        for(int i =0;i<10;i++){
-            AgentSkill skill = new AgentSkill();
-            skill.setActive(1);
-            skill.setName("haha"+i);
-            skills.add(skill);
+    public void test3() throws ExecutionException, InterruptedException {
+        int size = 1000;
+        ExecutorService pool= Executors.newFixedThreadPool(50);
+        List<Future> results = new ArrayList<>();
+        for (int i =0;i<size;i++) {
+            final int j = i;
+            results.add(pool.submit(new Runnable() {
+                @Override
+                public void run() {
+                    CallCenterAgent callCenterAgent = new CallCenterAgent();
+                    callCenterAgent.setName("test1"+j);
+                    callCenterAgent.setExtentions(appExtensionService.pageList(2,1).getResult());
+                    callCenterAgent.setState(CallCenterAgent.STATE_IDLE);
+                    List<AgentSkill> skills = new ArrayList<>();
+                    for(int i =0;i<10;i++){
+                        AgentSkill skill = new AgentSkill();
+                        skill.setLevel(new Random().nextInt(100));
+                        skill.setActive(1);
+                        skill.setName("haha"+i);
+                        skills.add(skill);
+                    }
+                    callCenterAgent.setSkills(skills);
+                    String agentId = callCenterAgentService.login("40288ac9575612a30157561c7ff50004","40288ac957e1812e0157e18a994e0000",callCenterAgent);
+                }
+            }));
         }
-        callCenterAgent.setSkills(skills);
-        String agentId = callCenterAgentService.login("40288ac9575612a30157561c7ff50004","40288ac957e1812e0157e18a994e0000",callCenterAgent);
+        for (Future f: results) {
+            f.get();
+        }
+        pool.shutdown();
     }
 
     @Test
