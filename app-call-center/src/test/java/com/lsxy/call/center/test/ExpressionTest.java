c6f3bec3c6564a2c03dff17e4defcda481e22ff6
liuws@yunhuni.com <liuws@yunhuni.com>
2016-11-09 17:17:18 +0800

    表达式 运算 test

--- app-call-center/src/test/java/com/lsxy/call/center/test/ExpressionTest.java
+++ app-call-center/src/test/java/com/lsxy/call/center/test/ExpressionTest.java
@@ -10,6 +10,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * Created by liuws on 2016/11/8.
@@ -23,10 +24,23 @@ public class ExpressionTest {
         System.setProperty("spring.config.location","classpath:"+ Constants.DEFAULT_CONFIG_FILE);
     }
     @Test
-    public void test() {
-        Map<String,Integer> vars = new HashMap<>();
-        vars.put("haha0",11);
-        vars.put("haha1",22);
-        System.out.println(ExpressionUtils.execSortExpression("get(\"haha0\") + get(\"haha1\");",vars));
+    public void test() throws InterruptedException {
+        int size = 1000;
+        long start = System.currentTimeMillis();
+        CountDownLatch countDownLatch = new CountDownLatch(size);
+        for(int i=0;i<size;i++){
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    Map<String,Integer> vars = new HashMap<>();
+                    vars.put("haha0",11);
+                    vars.put("haha1",22);
+                    System.out.println(ExpressionUtils.execSortExpression("get(\"haha0\") + get(\"haha1\");",vars));
+                    countDownLatch.countDown();
+                }
+            }).start();
+        }
+        countDownLatch.await();
+        System.out.println(System.currentTimeMillis() - start);
     }
 }
