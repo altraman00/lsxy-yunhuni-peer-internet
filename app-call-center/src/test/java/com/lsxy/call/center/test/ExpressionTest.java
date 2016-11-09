4a82e216322b8af5d311f7e902796db36c7dc8f3
liuws@yunhuni.com <liuws@yunhuni.com>
2016-11-09 09:21:40 +0800

    expression test

--- app-call-center/src/test/java/com/lsxy/call/center/test/ExpressionTest.java
+++ app-call-center/src/test/java/com/lsxy/call/center/test/ExpressionTest.java
@@ -1,6 +1,12 @@
 package com.lsxy.call.center.test;
 
+import com.lsxy.call.center.CallCenterMainClass;
 import com.lsxy.call.center.utils.ExpressionUtils;
+import com.lsxy.framework.config.Constants;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -8,10 +14,16 @@ import java.util.Map;
 /**
  * Created by liuws on 2016/11/8.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(CallCenterMainClass.class)
 public class ExpressionTest {
 
-    public static void main(String[] args) {
-
+    static {
+        //将 spring boot 的默认配置文件设置为系统配置文件
+        System.setProperty("spring.config.location","classpath:"+ Constants.DEFAULT_CONFIG_FILE);
+    }
+    @Test
+    public void test() {
         Map<String,Integer> vars = new HashMap<>();
         vars.put("haha0",11);
         vars.put("haha1",22);
