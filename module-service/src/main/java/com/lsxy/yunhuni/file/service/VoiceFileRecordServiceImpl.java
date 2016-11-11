1d56a76df9b58b79a597665586905ed2c81f6b06
zhangxb <295235027@qq.com>
2016-11-11 14:16:27 +0800

    修改

--- module-service/src/main/java/com/lsxy/yunhuni/file/service/VoiceFileRecordServiceImpl.java
+++ module-service/src/main/java/com/lsxy/yunhuni/file/service/VoiceFileRecordServiceImpl.java
@@ -7,7 +7,9 @@ import com.lsxy.yunhuni.api.file.model.VoiceFilePlay;
 import com.lsxy.yunhuni.api.file.model.VoiceFileRecord;
 import com.lsxy.yunhuni.api.file.service.VoiceFileRecordService;
 import com.lsxy.yunhuni.api.product.enums.ProductCode;
+import com.lsxy.yunhuni.api.session.model.MeetingMember;
 import com.lsxy.yunhuni.api.session.model.VoiceCdr;
+import com.lsxy.yunhuni.api.session.service.MeetingMemberService;
 import com.lsxy.yunhuni.api.session.service.VoiceCdrService;
 import com.lsxy.yunhuni.file.dao.VoiceFileRecordDao;
 import org.apache.commons.lang.StringUtils;
@@ -36,6 +38,8 @@ public class VoiceFileRecordServiceImpl extends AbstractService<VoiceFileRecord>
     private JdbcTemplate jdbcTemplate;
     @Autowired
     VoiceCdrService voiceCdrService;
+    @Autowired
+    MeetingMemberService meetingMemberService;
 
     @Override
     public Page<VoiceFileRecord> pageList(Integer pageNo, Integer pageSize,String appId,String tenantId) {
@@ -79,23 +83,35 @@ public class VoiceFileRecordServiceImpl extends AbstractService<VoiceFileRecord>
             ProductCode p1 = ProductCode.changeApiCmdToProductCode(voiceCdr.getType());
             //语音会议
             if(ProductCode.sys_conf.getRemark().equals(p1.getRemark())){
-
+                //获取会议操作者
+                MeetingMember meetingMember = meetingMemberService.findById(voiceCdr.getSessionId());
+                String hql = "  FROM VoiceFileRecord obj WHERE obj.sessionId=?1 ";
+                //使用会议id
+                List list= this.list(hql,meetingMember.getMeeting().getId());
+                return list;
             }
             //自定义IVR
             else if(ProductCode.ivr_call.getRemark().equals(p1.getRemark())){
-
+                String hql = "  FROM VoiceFileRecord obj WHERE obj.sessionId=?1 ";
+                //使用ivr的id
+                List list= this.list(hql,voiceCdr.getSessionId());
+                return list;
             }
             //语音回拔
             else if(ProductCode.duo_call.getRemark().equals(p1.getRemark())){
-                String hql = "  FROM VoiceFileRecord obj WHERE obj.sessionId=?1 AND obj.sessionCode=?2 ";
-                List list= this.list(hql,voiceCdr.getSessionId(),voiceCdr.getType());
+                String hql = "  FROM VoiceFileRecord obj WHERE obj.sessionId=?1 ";
+                //使用双向回拨的id
+                List list= this.list(hql,voiceCdr.getSessionId());
                 return list;
             }
             //呼叫中心
             else if(ProductCode.call_center.getRemark().equals(p1.getRemark())){
-
+                //根据sessionid获取呼叫中心交互成员，在获取呼叫中心交谈，在获取文件
+                String hql = "  FROM VoiceFileRecord obj WHERE obj.sessionId=?1 ";
+                //使用ivr的id
+                List list= this.list(hql,voiceCdr.getSessionId());
+                return list;
             }
-
         }
         return null;
     }
