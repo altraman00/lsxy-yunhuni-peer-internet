c0ad81191903a29bb2a68230b7c42acee854e332
liuws@yunhuni.com <liuws@yunhuni.com>
2016-11-21 17:51:34 +0800

    呼叫振铃事件  加入交谈

--- app-area-server/src/main/java/com/lsxy/area/server/event/handler/call/Handler_EVENT_SYS_CALL_ON_RINGING.java
+++ app-area-server/src/main/java/com/lsxy/area/server/event/handler/call/Handler_EVENT_SYS_CALL_ON_RINGING.java
@@ -2,18 +2,14 @@ package com.lsxy.area.server.event.handler.call;
 
 import com.lsxy.area.api.BusinessState;
 import com.lsxy.area.api.BusinessStateService;
-import com.lsxy.area.api.ConfService;
 import com.lsxy.area.server.event.EventHandler;
 import com.lsxy.area.server.service.callcenter.ConversationService;
-import com.lsxy.area.server.util.NotifyCallbackUtil;
+import com.lsxy.framework.core.exceptions.api.YunhuniApiException;
 import com.lsxy.framework.rpc.api.RPCRequest;
 import com.lsxy.framework.rpc.api.RPCResponse;
 import com.lsxy.framework.rpc.api.event.Constants;
 import com.lsxy.framework.rpc.api.session.Session;
 import com.lsxy.framework.rpc.exceptions.InvalidParamException;
-import com.lsxy.yunhuni.api.app.service.AppService;
-import com.lsxy.yunhuni.api.session.model.CallSession;
-import com.lsxy.yunhuni.api.session.service.CallSessionService;
 import org.apache.commons.collections.MapUtils;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
@@ -27,32 +23,19 @@ import java.util.Map;
  * Created by liuws on 2016/8/29.
  */
 @Component
-public class Handler_EVENT_SYS_CALL_ON_START extends EventHandler{
+public class Handler_EVENT_SYS_CALL_ON_RINGING extends EventHandler{
 
-    private static final Logger logger = LoggerFactory.getLogger(Handler_EVENT_SYS_CALL_ON_START.class);
+    private static final Logger logger = LoggerFactory.getLogger(Handler_EVENT_SYS_CALL_ON_RINGING.class);
 
     @Autowired
     private BusinessStateService businessStateService;
 
     @Autowired
-    private AppService appService;
-
-    @Autowired
-    private ConfService confService;
-
-    @Autowired
-    private CallSessionService callSessionService;
-
-    @Autowired
     private ConversationService conversationService;
-
-    @Autowired
-    private NotifyCallbackUtil notifyCallbackUtil;
-
 
     @Override
     public String getEventName() {
-        return Constants.EVENT_SYS_CALL_ON_START;
+        return Constants.EVENT_SYS_CALL_ON_RINGING;
     }
 
     @Override
@@ -79,12 +62,15 @@ public class Handler_EVENT_SYS_CALL_ON_START extends EventHandler{
             logger.info("call_id={},state={}",call_id,state);
         }
 
-        //更新会话记录状态
-        CallSession callSession = callSessionService.findById((String)state.getBusinessData().get("sessionid"));
-        if(callSession != null){
-            callSession.setResId(res_id);
-            callSession.setStatus(CallSession.STATUS_CALLING);
-            callSessionService.save(callSession);
+        if(BusinessState.TYPE_CC_AGENT_CALL.equals(state.getType())){
+            Map<String,Object> businessData = state.getBusinessData();
+            //加入交谈
+            String conversation = (String)businessData.get(ConversationService.CONVERSATION_ID);
+            try {
+                conversationService.join(state.getAppId(),conversation,call_id,null,null,null);
+            } catch (YunhuniApiException e) {
+                logger.error("将呼叫加入交谈失败",e);
+            }
         }
         return res;
     }
