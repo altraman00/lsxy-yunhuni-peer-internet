9074f83a49782eefaa3cf0b637f0149d1395f325
liuws@yunhuni.com <liuws@yunhuni.com>
2016-11-22 14:40:45 +0800

    文件编码错误

--- module-service-call-center-api/src/main/java/com/lsxy/call/center/api/service/CallCenterConversationMemberService.java
+++ module-service-call-center-api/src/main/java/com/lsxy/call/center/api/service/CallCenterConversationMemberService.java
@@ -4,7 +4,9 @@ import com.lsxy.call.center.api.model.CallCenterConversationMember;
 import com.lsxy.framework.api.base.BaseService;
 
 /**
+ * 交谈成员表
  * Created by liuws on 2016/11/18.
  */
 public interface CallCenterConversationMemberService extends BaseService<CallCenterConversationMember> {
+	
 }
