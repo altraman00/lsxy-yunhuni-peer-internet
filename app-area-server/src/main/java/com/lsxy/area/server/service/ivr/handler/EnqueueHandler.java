913baf5dbfaf95865dcfbd0deca044aac75cbff4
liuws@yunhuni.com <liuws@yunhuni.com>
2016-11-23 14:10:02 +0800

    test

--- app-area-server/src/main/java/com/lsxy/area/server/service/ivr/handler/EnqueueHandler.java
+++ app-area-server/src/main/java/com/lsxy/area/server/service/ivr/handler/EnqueueHandler.java
@@ -129,10 +129,4 @@ public class EnqueueHandler extends ActionHandler{
         }
         return true;
     }
-
-    public static void main(String[] args) {
-        Integer a = new Integer(1);
-        Integer b = new Integer(1);
-        System.out.println(a == b);
-    }
 }
