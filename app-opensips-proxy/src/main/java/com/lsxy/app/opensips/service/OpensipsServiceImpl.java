2a09285ccc42089a75f2da253feb1a1755b149bb
liups <liups@yunhuni.com>
2016-11-25 18:03:21 +0800

    YHNI-235 分机参数判断

--- app-opensips-proxy/src/main/java/com/lsxy/app/opensips/service/OpensipsServiceImpl.java
+++ app-opensips-proxy/src/main/java/com/lsxy/app/opensips/service/OpensipsServiceImpl.java
@@ -9,6 +9,7 @@ import com.lsxy.call.center.api.opensips.service.OpensipsService;
 import com.lsxy.framework.config.SystemConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 
 import java.math.BigInteger;
 import java.security.MessageDigest;
@@ -28,29 +29,10 @@ public class OpensipsServiceImpl implements OpensipsService {
 
     @Override
     public void createExtension(String username, String password) {
-        String ha1 = getMD5(username + ":" + domain + ":" + password);
-        String ha1b = getMD5(username + "@" + domain + ":" + domain + ":" + password);
+        String ha1 = DigestUtils.md5DigestAsHex((username + ":" + domain + ":" + password).getBytes());
+        String ha1b = DigestUtils.md5DigestAsHex((username + "@" + domain + ":" + domain + ":" + password).getBytes());
         Subscriber subscriber = new Subscriber(username,domain,password,ha1,ha1b);
         subscriberDao.save(subscriber);
-    }
-
-    /**
-     * 对字符串md5加密
-     * @param str
-     * @return
-     */
-    private static String getMD5(String str) {
-        try {
-            // 生成一个MD5加密计算摘要
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            // 计算md5函数
-            md.update(str.getBytes());
-            // digest()最后确定返回md5 hash值，返回值为8为字符串。因为md5 hash值是16位的hex值，实际上就是8位的字符
-            // BigInteger函数则将8位的字符串转换成16位hex值，用字符串来表示；得到字符串形式的hash值
-            return new BigInteger(1, md.digest()).toString(16);
-        } catch (Exception e) {
-            throw new RuntimeException("MD5加密出现错误",e);
-        }
     }
 
     @Override
