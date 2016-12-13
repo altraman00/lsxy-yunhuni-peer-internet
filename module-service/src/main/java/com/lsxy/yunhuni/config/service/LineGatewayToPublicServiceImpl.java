533473b15f1e771e73756a17af238ed2381f9e95
zhangxb <zhangxb@yunhuni.com>
2016-12-13 14:14:10 +0800

    修正删除最后一个共有线路时序号发生改变

--- module-service/src/main/java/com/lsxy/yunhuni/config/service/LineGatewayToPublicServiceImpl.java
+++ module-service/src/main/java/com/lsxy/yunhuni/config/service/LineGatewayToPublicServiceImpl.java
@@ -137,6 +137,7 @@ public class LineGatewayToPublicServiceImpl extends AbstractService<LineGatewayT
 
     @Override
     public void removePublic(String id) {
+        int o3 = this.getMaxPriority();
         LineGatewayToPublic lineGatewayToPublic = this.findById(id);
         //删除线路关系
         try {
@@ -151,7 +152,6 @@ public class LineGatewayToPublicServiceImpl extends AbstractService<LineGatewayT
             lineGatewayService.save(lineGateway);
         }
         //修正优先级
-        int o3 = this.getMaxPriority();
         if(o3!=lineGatewayToPublic.getPriority()){
             int re = upPriority(lineGatewayToPublic.getPriority(),o3,null);
             if(re==-1){
