faca2083a78d5351d1725d1f07b87664d5ff7fab
liuws <liuws@yunhuni.com>
2016-12-15 19:37:25 +0800

    call_id is null 去掉抛出异常

--- app-area-server/src/main/java/com/lsxy/area/server/event/handler/call/Handler_EVENT_SYS_CALL_ON_RELEASE.java
+++ app-area-server/src/main/java/com/lsxy/area/server/event/handler/call/Handler_EVENT_SYS_CALL_ON_RELEASE.java
@@ -102,7 +102,9 @@ public class Handler_EVENT_SYS_CALL_ON_RELEASE extends EventHandler{
         String call_id = (String)params.get("user_data");
 
         if(StringUtils.isBlank(call_id)){
-            throw new InvalidParamException("call_id is null");
+            //throw new InvalidParamException("call_id is null");
+            logger.info("call_id is null");
+            return null;
         }
 
         BusinessState state = businessStateService.get(call_id);
