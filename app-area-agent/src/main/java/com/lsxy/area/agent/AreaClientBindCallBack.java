d8bd4da0350f4bdcd48cfabd416d824ea193d639
谭畅 <tanc@yunhuni.com>
2016-12-15 14:25:45 +0800

    恢复中

--- app-area-agent/src/main/java/com/lsxy/area/agent/AreaClientBindCallBack.java
+++ app-area-agent/src/main/java/com/lsxy/area/agent/AreaClientBindCallBack.java
@@ -24,7 +24,7 @@ public class AreaClientBindCallBack implements ClientBindCallback {
     @Override
     public void doCallback(Session session) throws Exception {
         logger.info("连接服务器成功，开始进行压测");
-        int threads = 100;
+        int threads = 1;
         int count = 10000000;
         doTestRequest(threads,count,session);
     }
