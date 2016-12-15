1f1817aaec1866a5cd398ffb91059d8b784b8b0f
liuws <liuws@yunhuni.com>
2016-12-15 20:25:50 +0800

    去掉 response

--- app-area-agent/src/main/java/com/lsxy/area/agent/handler/conf/Handler_MN_CH_SYS_CONF_PLAY_STOP.java
+++ app-area-agent/src/main/java/com/lsxy/area/agent/handler/conf/Handler_MN_CH_SYS_CONF_PLAY_STOP.java
@@ -36,13 +36,7 @@ public class Handler_MN_CH_SYS_CONF_PLAY_STOP extends RpcRequestHandler{
 
     @Override
     public RPCResponse handle(RPCRequest request, Session session) {
-        RPCResponse response = RPCResponse.buildResponse(request);
-
         Commander cticlient = cticlientContext.getAvalibleClient();
-        if(cticlient == null) {
-            response.setMessage(RPCResponse.STATE_EXCEPTION);
-            return response;
-        }
 
         Map<String, Object> params = request.getParamMap();
         String conf_id = (String)params.get("user_data");
@@ -68,12 +62,10 @@ public class Handler_MN_CH_SYS_CONF_PLAY_STOP extends RpcRequestHandler{
                     logger.error("调用sys.conf.play_stop超时conf_id={}",conf_id);
                 }
             });
-            response.setMessage(RPCResponse.STATE_OK);
-        } catch (IOException e) {
+        } catch (Throwable e) {
             logger.error("调用资源操作失败",e);
-            response.setMessage(RPCResponse.STATE_EXCEPTION);
         }
-        return response;
+        return null;
 
     }
 }
