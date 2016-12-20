fa5ee04cdfb9ccde3731fd979039b16bbeaf6cbe
liuws <liuws@yunhuni.com>
2016-12-20 18:23:22 +0800

    出发重启

--- app-area-server/src/main/java/com/lsxy/area/server/mq/handler/CdrEventHandler.java
+++ app-area-server/src/main/java/com/lsxy/area/server/mq/handler/CdrEventHandler.java
@@ -68,7 +68,7 @@ public class CdrEventHandler implements MQMessageHandler<CdrEvent> {
         }
 
         if(logger.isDebugEnabled()){
-            logger.debug("插入cdr数据：{}", JSONUtil.objectToJson(voiceCdr));
+            logger.debug("插入cdr数据： {}", JSONUtil.objectToJson(voiceCdr));
         }
         calBillingService.incCallSum(voiceCdr.getTenantId(),voiceCdr.getCallEndDt());
         if(voiceCdr.getCallAckDt() != null){
