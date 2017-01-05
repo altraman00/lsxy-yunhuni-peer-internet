c21bfa319027a8354b93267c2a71c15f0cd11e13
liuws <liuws@yunhuni.com>
2017-01-05 16:08:16 +0800

    循环播放排队等待音

--- app-area-server/src/main/java/com/lsxy/area/server/service/ivr/handler/EnqueueHandler.java
+++ app-area-server/src/main/java/com/lsxy/area/server/service/ivr/handler/EnqueueHandler.java
@@ -83,7 +83,8 @@ public class EnqueueHandler extends ActionHandler{
                             .putIfNotEmpty("res_id",state.getResId())
                             .putIfNotEmpty("content", JSONUtil2.objectToJson(new Object[][]{new Object[]{playWait,0,""}}))
                             .putIfNotEmpty("user_data",callId)
-                            .put("finish_keys","")
+                            .put("finish_keys","")//不允许按键打断
+                            .put("repeat",99)
                             .put("areaId",state.getAreaId())
                             .build();
                     RPCRequest rpcrequest = RPCRequest.newRequest(ServiceConstants.MN_CH_SYS_CALL_PLAY_START, params);
