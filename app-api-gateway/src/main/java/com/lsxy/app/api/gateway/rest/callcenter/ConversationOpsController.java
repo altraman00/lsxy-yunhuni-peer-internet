a5e9e55fc74dda48f4c6f0974d3662c88a90a65a
liuws <liuws@yunhuni.com>
2017-01-10 13:56:45 +0800

    设置坐席收放音模式

--- app-api-gateway/src/main/java/com/lsxy/app/api/gateway/rest/callcenter/ConversationOpsController.java
+++ app-api-gateway/src/main/java/com/lsxy/app/api/gateway/rest/callcenter/ConversationOpsController.java
@@ -1,9 +1,13 @@
 package com.lsxy.app.api.gateway.rest.callcenter;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.lsxy.app.api.gateway.dto.callcenter.SetVoiceModeInputDTO;
 import com.lsxy.app.api.gateway.response.ApiGatewayResponse;
 import com.lsxy.app.api.gateway.rest.AbstractAPIController;
 import com.lsxy.area.api.callcenter.ConversationOps;
+import com.lsxy.call.center.api.model.CallCenterAgent;
+import com.lsxy.call.center.api.service.CallCenterAgentService;
+import com.lsxy.framework.core.exceptions.api.AgentNotExistException;
 import com.lsxy.framework.core.exceptions.api.YunhuniApiException;
 import com.lsxy.framework.web.utils.WebUtils;
 import org.slf4j.Logger;
@@ -11,6 +15,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 /**
  * Created by liuws on 2016/11/14.
@@ -23,6 +28,9 @@ public class ConversationOpsController extends AbstractAPIController {
     @Reference(timeout=3000,check = false,lazy = true)
     private ConversationOps conversationOps;
 
+    @Reference(timeout=3000,check = false,lazy = true)
+    private CallCenterAgentService callCenterAgentService;
+
     @RequestMapping(value = "/{accountId}/callcenter/conversation/{id}",method = RequestMethod.DELETE)
     public ApiGatewayResponse save(HttpServletRequest request,@PathVariable String accountId, @PathVariable String id,
                                    @RequestHeader(value = "AppID") String appId) throws YunhuniApiException {
@@ -31,6 +39,22 @@ public class ConversationOpsController extends AbstractAPIController {
         }
         String ip = WebUtils.getRemoteAddress(request);
         boolean result = conversationOps.dismiss(ip,appId,id);
+        return ApiGatewayResponse.success(result);
+    }
+
+    @RequestMapping(value = "/{accountId}/callcenter/conversation/{id}/agent/{name}/lsm",method = RequestMethod.POST)
+    public ApiGatewayResponse lsm(HttpServletRequest request,@PathVariable String accountId, @PathVariable String id,
+                                  @PathVariable String name,@RequestHeader(value = "AppID") String appId,
+                                  @Valid @RequestBody SetVoiceModeInputDTO dto) throws YunhuniApiException {
+        if(logger.isDebugEnabled()){
+            logger.debug("CONVERSATION DISMISS API参数,accountId={},appId={},dto={}",accountId,appId,id);
+        }
+        String ip = WebUtils.getRemoteAddress(request);
+        CallCenterAgent agent = callCenterAgentService.get(appId,name);
+        if(agent == null){
+            throw new AgentNotExistException();
+        }
+        boolean result = conversationOps.setVoiceMode(ip,appId,id,agent.getId(),dto.getMode());
         return ApiGatewayResponse.success(result);
     }
 }
