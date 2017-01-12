b32ce067d01e3ba8490d4713ace9eea36f0c9fb9
liups <liups@yunhuni.com>
2017-01-12 10:30:17 +0800

    YHNI-297 应用绑定号码接口

--- app-oc/src/main/java/com/lsxy/app/oc/rest/tenant/vo/AppNumVO.java
+++ app-oc/src/main/java/com/lsxy/app/oc/rest/tenant/vo/AppNumVO.java
@@ -1,17 +1,26 @@
 package com.lsxy.app.oc.rest.tenant.vo;
 
+import io.swagger.annotations.ApiModelProperty;
+
 import java.util.Date;
 
 /**
  * Created by liups on 2017/1/11.
  */
 public class AppNumVO {
+    @ApiModelProperty(name = "rentId",value = "租用记录Id")
     String rentId;
+    @ApiModelProperty(name = "num",value = "号码")
     String num;
+    @ApiModelProperty(name = "status",value = "状态：0、过期，1、正常")
     String status;
+    @ApiModelProperty(name = "isCalled",value = "是否可呼入")
     String isCalled;
+    @ApiModelProperty(name = "isDialing",value = "是否可呼出")
     String isDialing;
+    @ApiModelProperty(name = "areaCode",value = "区号")
     String areaCode;
+    @ApiModelProperty(name = "expireTime",value = "过期时间")
     Date expireTime;
 
     public String getRentId() {
