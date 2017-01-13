96dd085cf2bb03fda5b9e15fde46167117c14674
liups <liups@yunhuni.com>
2017-01-13 17:28:12 +0800

    YHNI-332 修改extensionVO和agentVO的创建方式

--- app-api-gateway/src/main/java/com/lsxy/app/api/gateway/rest/callcenter/vo/AgentVO.java
+++ app-api-gateway/src/main/java/com/lsxy/app/api/gateway/rest/callcenter/vo/AgentVO.java
@@ -1,5 +1,9 @@
 package com.lsxy.app.api.gateway.rest.callcenter.vo;
 
+import com.lsxy.call.center.api.model.AgentSkill;
+import com.lsxy.call.center.api.model.CallCenterAgent;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,6 +16,31 @@ public class AgentVO {
     private String state;
     private String extension;
     private List skills;
+
+    public AgentVO() {
+    }
+
+    public AgentVO(String name, String channel, String num, String state, String extension, List skills) {
+        this.name = name;
+        this.channel = channel;
+        this.num = num;
+        this.state = state;
+        this.extension = extension;
+        this.skills = skills;
+    }
+
+    public static AgentVO changeCallCenterAgentToAgentVO(CallCenterAgent agent){
+        List<AgentSkill> skills = agent.getSkills();
+        List<AgentSkillVO> skillVos = null;
+        if(skills != null){
+            skillVos = new ArrayList<>();
+            for(AgentSkill skill:skills){
+                AgentSkillVO skillVO = AgentSkillVO.changeAgentSkillToAgentSkillVO(skill);
+                skillVos.add(skillVO);
+            }
+        }
+        return new AgentVO(agent.getName(),agent.getChannel(),agent.getNum(),agent.getState(),agent.getExtension(),skillVos);
+    }
 
     public String getName() {
         return name;
