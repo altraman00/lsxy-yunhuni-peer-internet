8e88161096f72d946d58d328bc1117d209baaa49
liups <liups@yunhuni.com>
2017-01-17 16:10:38 +0800

    TT-879 apigw处理Json转换错误JsonMappingException异常，返回参数错误信息

--- app-api-gateway/src/main/java/com/lsxy/app/api/gateway/rest/AbstractAPIController.java
+++ app-api-gateway/src/main/java/com/lsxy/app/api/gateway/rest/AbstractAPIController.java
@@ -1,8 +1,12 @@
 package com.lsxy.app.api.gateway.rest;
 
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lsxy.app.api.gateway.dto.DuoCallbackDTO;
 import com.lsxy.app.api.gateway.response.ApiGatewayResponse;
 import com.lsxy.framework.core.exceptions.api.ApiReturnCodeEnum;
 import com.lsxy.framework.core.exceptions.api.YunhuniApiException;
+import net.sf.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -38,7 +42,7 @@ public class AbstractAPIController {
         }
         if(resultEx instanceof YunhuniApiException){
             failed = ApiGatewayResponse.failed(((YunhuniApiException) resultEx).getCode(),resultEx.getMessage());
-        }else if(resultEx instanceof MethodArgumentNotValidException){
+        }else if(resultEx instanceof MethodArgumentNotValidException || resultEx instanceof JsonMappingException){
             failed = ApiGatewayResponse.failed(ApiReturnCodeEnum.IllegalArgument.getCode(), ApiReturnCodeEnum.IllegalArgument.getMsg());
         }else{
             failed = ApiGatewayResponse.failed(ApiReturnCodeEnum.UnknownFail.getCode(), ApiReturnCodeEnum.UnknownFail.getMsg());
