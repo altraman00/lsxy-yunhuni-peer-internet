a06861365d2f6756d0a4fa16efcf709c09c91c95
liups <liups@yunhuni.com>
2017-02-09 15:23:38 +0800

    YHNI-331 用户中心分机座席列表

--- app-api-gateway/src/main/java/com/lsxy/app/api/gateway/rest/callcenter/vo/ExtensionVO.java
+++ app-api-gateway/src/main/java/com/lsxy/app/api/gateway/rest/callcenter/vo/ExtensionVO.java
@@ -12,21 +12,23 @@ public class ExtensionVO {
     private String password;//    password             varchar(50) comment 'SIP注册密码',
     private String ipaddr;  //SIP 网关IP地址与端口，默认5060，仅用于 type==2的情况
     private String telnum;//    telenum              varchar(32) comment '如果是电话分机，该属性记录电话号码（保留，不用）',
+    private Boolean enable; //分机是否可用（不存到数据库）;
 
     public ExtensionVO() {
     }
 
-    public ExtensionVO(String id, String type, String user, String password, String ipaddr, String telnum) {
+    public ExtensionVO(String id, String type, String user, String password, String ipaddr, String telnum,Boolean enable) {
         this.id = id;
         this.type = type;
         this.user = user;
         this.password = password;
         this.ipaddr = ipaddr;
         this.telnum = telnum;
+        this.enable = enable;
     }
 
     public static ExtensionVO changeAppExtensionToExtensionVO(AppExtension ext){
-        return new ExtensionVO(ext.getId(),ext.getType(),ext.getUser(),ext.getPassword(),ext.getIpaddr(),ext.getTelnum());
+        return new ExtensionVO(ext.getId(),ext.getType(),ext.getUser(),ext.getPassword(),ext.getIpaddr(),ext.getTelnum(),ext.getEnable());
     }
 
     public String getId() {
@@ -75,5 +77,13 @@ public class ExtensionVO {
 
     public void setTelnum(String telnum) {
         this.telnum = telnum;
+    }
+
+    public Boolean getEnable() {
+        return enable;
+    }
+
+    public void setEnable(Boolean enable) {
+        this.enable = enable;
     }
 }
--- app-oc/src/main/java/com/lsxy/app/oc/rest/tenant/vo/AgentSkillVO.java
+++ app-oc/src/main/java/com/lsxy/app/oc/rest/tenant/vo/AgentSkillVO.java
@@ -1,13 +1,17 @@
 package com.lsxy.app.oc.rest.tenant.vo;
 
 import com.lsxy.call.center.api.model.AgentSkill;
+import io.swagger.annotations.ApiModelProperty;
 
 /**
  * Created by liups on 2016/11/18.
  */
 public class AgentSkillVO {
+    @ApiModelProperty(name="name",value = "技能名称")
     private String name;
+    @ApiModelProperty(name="score",value = "技能分数")
     private Integer score;
+    @ApiModelProperty(name="enabled",value = "是否可用")
     private Boolean enabled;
 
     public AgentSkillVO() {
