f842a798405c03e2f40535141f89ad12031e50da
liuws <liuws@yunhuni.com>
2017-02-09 17:22:40 +0800

    指定坐席排队

--- app-area-server/src/main/java/com/lsxy/area/server/service/callcenter/api/ConversationOps.java
+++ app-area-server/src/main/java/com/lsxy/area/server/service/callcenter/api/ConversationOps.java
@@ -216,7 +216,7 @@ public class ConversationOps implements com.lsxy.call.center.api.service.Convers
                         .putIfNotEmpty(CallCenterUtil.CALLCENTER_FIELD,conversation_state.getBusinessData().get(CallCenterUtil.CALLCENTER_FIELD))
                         .putIfNotEmpty(CallCenterUtil.ENQUEUE_START_TIME_FIELD,""+new Date().getTime())
                         .putIfNotEmpty(CallCenterUtil.CHANNEL_ID_FIELD,enQueue.getChannel())
-                        .putIfNotEmpty(CallCenterUtil.CONDITION_ID_FIELD,enQueue.getRoute().getCondition().getId())
+                        .putIfNotEmpty(CallCenterUtil.CONDITION_ID_FIELD,enQueue.getRoute().getCondition()!=null?enQueue.getRoute().getCondition().getId():null)
                         .putIfNotEmpty("user_data",enQueue.getData())
                         .build())
                 .build();
