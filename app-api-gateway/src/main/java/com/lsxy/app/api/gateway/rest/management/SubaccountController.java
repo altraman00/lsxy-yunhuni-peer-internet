f69daff54e4d80a19505cf39d6c7d412439fcb47
liuws <liuws@yunhuni.com>
2017-02-16 17:01:07 +0800

    子账号列表  详情

--- app-api-gateway/src/main/java/com/lsxy/app/api/gateway/rest/management/SubaccountController.java
+++ app-api-gateway/src/main/java/com/lsxy/app/api/gateway/rest/management/SubaccountController.java
@@ -6,6 +6,7 @@ import com.lsxy.app.api.gateway.rest.AbstractAPIController;
 import com.lsxy.framework.core.exceptions.api.AppServiceInvalidException;
 import com.lsxy.framework.core.exceptions.api.IPNotInWhiteListException;
 import com.lsxy.framework.core.exceptions.api.YunhuniApiException;
+import com.lsxy.framework.core.utils.Page;
 import com.lsxy.framework.web.utils.WebUtils;
 import com.lsxy.yunhuni.api.apicertificate.model.ApiCertificateSubAccount;
 import com.lsxy.yunhuni.api.apicertificate.model.CertAccountQuota;
@@ -13,6 +14,8 @@ import com.lsxy.yunhuni.api.apicertificate.service.ApiCertificateSubAccountServi
 import com.lsxy.yunhuni.api.apicertificate.service.CertAccountQuotaService;
 import com.lsxy.yunhuni.api.app.model.App;
 import com.lsxy.yunhuni.api.app.service.AppService;
+import com.lsxy.yunhuni.api.resourceTelenum.model.ResourceTelenum;
+import com.lsxy.yunhuni.api.resourceTelenum.model.ResourcesRent;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -89,7 +92,7 @@ public class SubaccountController extends AbstractAPIController {
         subAccount.setEnabled(dto.getEnabled());
         subAccount.setCallbackUrl(dto.getCallbackUrl());
         subAccount.setRemark(dto.getRemark());
-        subAccount = apiCertificateSubAccountService.updateSubAccount(subAccount);
+        apiCertificateSubAccountService.updateSubAccount(subAccount);
         return ApiGatewayResponse.success(subAccount);
     }
 
@@ -145,5 +148,54 @@ public class SubaccountController extends AbstractAPIController {
         certAccountQuotaService.updateQuotas(id,quotas);
         return ApiGatewayResponse.success(true);
     }
+
+
+    @RequestMapping(value = "/{accountId}/management/subaccount",method = RequestMethod.GET)
+    public ApiGatewayResponse pagelist(HttpServletRequest request,
+                                       @PathVariable String accountId,
+                                       @RequestHeader(value = "AppID") String appId,
+                                       @RequestParam(defaultValue = "1") Long pageNo,
+                                       @RequestParam(defaultValue = "10") Long pageSize
+    ) throws YunhuniApiException {
+        if(logger.isDebugEnabled()){
+            logger.debug("子账号列表API参数,accountId={},appId={},pageNo={},pageSize={}",accountId,appId,pageNo,pageSize);
+        }
+        String ip = WebUtils.getRemoteAddress(request);
+        App app = appService.findById(appId);
+        String whiteList = app.getWhiteList();
+        if(StringUtils.isNotBlank(whiteList)){
+            if(!whiteList.contains(ip)){
+                throw new IPNotInWhiteListException();
+            }
+        }
+        Page<ApiCertificateSubAccount> pageDatas = apiCertificateSubAccountService
+                .pageListWithNotQuota(appId,pageNo.intValue(),pageSize.intValue());
+
+        return ApiGatewayResponse.success(null);
+    }
+
+    @RequestMapping(value = "/{accountId}/management/subaccount/{id}",method = RequestMethod.GET)
+    public ApiGatewayResponse detail(HttpServletRequest request,
+                                       @PathVariable String accountId,
+                                       @RequestHeader(value = "AppID") String appId,
+                                       @PathVariable String id
+    ) throws YunhuniApiException {
+        if(logger.isDebugEnabled()){
+            logger.debug("子账号详情API参数,accountId={},appId={},id={}",accountId,appId,id);
+        }
+        String ip = WebUtils.getRemoteAddress(request);
+        App app = appService.findById(appId);
+        String whiteList = app.getWhiteList();
+        if(StringUtils.isNotBlank(whiteList)){
+            if(!whiteList.contains(ip)){
+                throw new IPNotInWhiteListException();
+            }
+        }
+        ApiCertificateSubAccount subAccount = apiCertificateSubAccountService.findByIdWithQuota(id);
+
+        return ApiGatewayResponse.success(null);
+    }
+
+
 }
 
