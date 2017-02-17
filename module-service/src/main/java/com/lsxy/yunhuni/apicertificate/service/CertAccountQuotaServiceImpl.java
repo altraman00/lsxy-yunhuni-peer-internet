39a7dccc94e6eed18f14337ea9a65725b7246265
liuws <liuws@yunhuni.com>
2017-02-17 17:22:07 +0800

    漏掉service注解

--- module-service/src/main/java/com/lsxy/yunhuni/apicertificate/service/CertAccountQuotaServiceImpl.java
+++ module-service/src/main/java/com/lsxy/yunhuni/apicertificate/service/CertAccountQuotaServiceImpl.java
@@ -6,6 +6,7 @@ import com.lsxy.yunhuni.api.apicertificate.model.CertAccountQuota;
 import com.lsxy.yunhuni.api.apicertificate.service.CertAccountQuotaService;
 import com.lsxy.yunhuni.apicertificate.dao.CertAccountQuotaDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.io.Serializable;
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.List;
 /**
  * Created by liups on 2017/2/15.
  */
+@Service
 public class CertAccountQuotaServiceImpl extends AbstractService<CertAccountQuota> implements CertAccountQuotaService {
     @Autowired
     CertAccountQuotaDao certAccountQuotaDao;
