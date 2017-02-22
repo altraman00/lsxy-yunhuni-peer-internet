c2504f9954507b8c2401ec13893ddfe528250eac
zhangxb <zhangxb@yunhuni.com>
2017-02-22 09:45:20 +0800

    详单下载

--- app-portal/src/main/java/com/lsxy/app/portal/console/statistics/SubAccountStatisticsController.java
+++ app-portal/src/main/java/com/lsxy/app/portal/console/statistics/SubAccountStatisticsController.java
@@ -16,12 +16,19 @@ import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.async.WebAsyncTask;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Callable;
 
 /**
  * Created by zhangxb on 2017/2/21.
@@ -75,5 +82,66 @@ public class SubAccountStatisticsController extends AbstractPortalController {
         mav.addObject("pageObj",result.getData());
         mav.setViewName("/console/statistics/subaccount/index");
         return mav;
+    }
+    @RequestMapping("{path}/download")
+    @ResponseBody
+    public String download(HttpServletRequest request, HttpServletResponse response, @PathVariable String path, String start, String end, String appId) {
+//        String oType = "";
+//        String title = "";
+//        String one = "";
+//        String[] headers = null;
+//        String[] values = null;
+//        String serviceType = "";
+//        if ("notify".equals(path)) {//语音通知
+//            oType = CallSession.TYPE_VOICE_NOTIFY;
+//            title = "语音通知";
+//            headers = new String[]{"子账号", "子账号密钥", "所属应用", "话务量（分钟）", "消费金额（元）","语音总用量 /配额（分钟）","坐席数/配额（个）"};
+//            values = new String[]{"callStartDt", "fromNum", "toNum", "cost", "costTimeLong"};
+//            serviceType = App.PRODUCT_VOICE;
+//        } else if ("code".equals(path)) {//语音验证码
+//            oType = CallSession.TYPE_VOICE_VOICECODE;
+//            title = "语音验证码";
+//            headers = new String[]{"发送时间", "主叫", "被叫", "挂机时间", "消费金额", "时长（秒）"};
+//            values = new String[]{"callStartDt", "fromNum", "toNum", "callEndDt", "cost", "costTimeLong"};
+//            serviceType = App.PRODUCT_VOICE;
+//        } else if ("ivr".equals(path)) {// 自定义IVR
+//            oType = CallSession.TYPE_VOICE_IVR;
+//            title = " 自定义IVR";
+//            headers = new String[]{"呼叫时间", "呼叫类型", "主叫", "被叫", "消费金额", "时长（秒）"};
+//            values = new String[]{"callStartDt", "ivrType:1=呼入;2=呼出", "fromNum", "toNum", "cost", "costTimeLong"};
+//            serviceType = App.PRODUCT_VOICE;
+//        } else if ("metting".equals(path)) {// 语音会议
+//            oType = CallSession.TYPE_VOICE_MEETING;
+//            title = " 语音会议";
+//            headers = new String[]{"会议标识ID", "呼叫时间", "参与者", "参与类型", "消费金额", "时长（秒）"};
+//            values = new String[]{"sessionId", "callStartDt", "joinType:0-fromNum;1-toNum;2-fromNum", "joinType:0=创建;1=邀请加入;2=呼入加入", "cost", "costTimeLong"};
+//            serviceType = App.PRODUCT_VOICE;
+//        } else if ("callback".equals(path)) {//语音回拨
+//            oType = CallSession.TYPE_VOICE_CALLBACK;
+//            title = "语音回拨";
+//            headers = new String[]{"呼叫时间", "主叫", "被叫", "消费金额", "时长（秒）"};
+//            values = new String[]{"callStartDt", "fromNum", "toNum", "cost", "costTimeLong"};
+//            serviceType = App.PRODUCT_VOICE;
+//        } else if ("callcenter".equals(path)) {
+//            oType = CallSession.TYPE_CALL_CENTER;
+//            title = "呼叫中心";
+//            headers = new String[]{"呼叫时间", "呼叫类型", "主叫", "被叫", "消费金额", "时长（秒）"};
+//            values = new String[]{"callStartDt", "ivrType:1=呼入;2=呼出", "fromNum", "toNum", "cost", "costTimeLong"};
+//            serviceType = App.PRODUCT_CALL_CENTER;
+//        }
+//        List list = null;
+//        if (StringUtils.isNotEmpty(oType)) {
+//            Map<String, String> map = init(request, start, end, appId, serviceType);
+//            list = (List) getList(request, oType, map.get("start"), map.get("end"), map.get("appId")).getData();
+//        }
+//        String appName = "";
+//        if (StringUtils.isNotEmpty(appId)) {
+//            appName = ((App) getAppById(request, appId).getData()).getName();
+//        } else {
+//            appName = "全部";
+//        }
+//        one = title + " 时间：" + start + "到" + end + ",应用：" + appName;
+//        downloadExcel(title, one, headers, values, list, null, "cost", response);
+        return "";
     }
 }
