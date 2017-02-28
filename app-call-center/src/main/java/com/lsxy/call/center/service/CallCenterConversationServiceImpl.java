fd1c90490569ade31378bf179a8eb0fe66a76d09
liuws <liuws@yunhuni.com>
2017-02-28 17:26:54 +0800

    子账号判断

--- app-call-center/src/main/java/com/lsxy/call/center/service/CallCenterConversationServiceImpl.java
+++ app-call-center/src/main/java/com/lsxy/call/center/service/CallCenterConversationServiceImpl.java
@@ -10,6 +10,7 @@ import com.lsxy.framework.api.base.BaseDaoInterface;
 import com.lsxy.framework.base.AbstractService;
 import com.lsxy.framework.core.exceptions.api.*;
 import com.lsxy.framework.core.utils.Page;
+import com.lsxy.yunhuni.api.apicertificate.service.ApiCertificateSubAccountService;
 import com.lsxy.yunhuni.api.app.model.App;
 import com.lsxy.yunhuni.api.app.service.AppService;
 import com.lsxy.yunhuni.api.app.service.ServiceType;
@@ -41,6 +42,9 @@ public class CallCenterConversationServiceImpl extends AbstractService<CallCente
     @Autowired
     private ApplicationContext applicationContext;
 
+    @Autowired
+    private ApiCertificateSubAccountService apiCertificateSubAccountService;
+
     @Override
     public BaseDaoInterface<CallCenterConversation, Serializable> getDao() {
         return callCenterConversationDao;
@@ -68,7 +72,7 @@ public class CallCenterConversationServiceImpl extends AbstractService<CallCente
         if(conversation == null){
             return null;
         }
-        if(subaccountId != conversation.getSubaccountId()){
+        if(!apiCertificateSubAccountService.subaccountCheck(subaccountId,conversation.getSubaccountId())){
             return null;
         }
         CallCenterConversationDetail detail = new CallCenterConversationDetail();
