70c1af418b2817dc573c614f3878c5c2a6576944
liups <liups@yunhuni.com>
2017-03-03 11:13:57 +0800

    YHNI-249 修改号码扣费逻辑

--- module-service/src/main/java/com/lsxy/yunhuni/resourceTelenum/service/ResourcesRentServiceImpl.java
+++ module-service/src/main/java/com/lsxy/yunhuni/resourceTelenum/service/ResourcesRentServiceImpl.java
@@ -237,15 +237,14 @@ public class ResourcesRentServiceImpl extends AbstractService<ResourcesRent> imp
             Date expireDate = DateUtils.getLastTimeOfMonth(new Date());
             ResourcesRent resourcesRent = new ResourcesRent(tenant,null,resourceTelenum,"号码资源",ResourcesRent.RESTYPE_TELENUM,new Date(),expireDate,ResourcesRent.RENT_STATUS_UNUSED);
             this.save(resourcesRent);
+            Consume consume = new Consume(new Date(), ConsumeCode.rent_number.name(),resourceTelenum.getAmount() , ConsumeCode.rent_number.getName(), "0", tenant.getId(),resourcesRent.getId());
+            consumeService.consume(consume);
+            //扣费
+            //号码月租费
+            BigDecimal cost = calCostService.calCost(ProductCode.rent_number_month.name(),tenant.getId());
+            Consume consume1 = new Consume(new Date(), ConsumeCode.rent_number_month.name(), cost, ConsumeCode.rent_number_month.getName(), "0", tenant.getId(),resourcesRent.getId());
+            consumeService.consume(consume1);
         }
-        //扣费
-        Consume consume = new Consume(new Date(), ConsumeCode.rent_number.name(), temp.getAmount(), ConsumeCode.rent_number.getName(), "0", tenant.getId(),null);
-        consumeService.consume(consume);
-        //号码月租费
-        BigDecimal cost = calCostService.calCost(ProductCode.rent_number_month.name(),tenant.getId());
-        BigDecimal bigDecimal = cost.multiply(new BigDecimal(list.size()));
-        Consume consume1 = new Consume(new Date(), ConsumeCode.rent_number_month.name(), bigDecimal, ConsumeCode.rent_number_month.getName(), "0", tenant.getId(),null);
-        consumeService.consume(consume1);
     }
 
 
