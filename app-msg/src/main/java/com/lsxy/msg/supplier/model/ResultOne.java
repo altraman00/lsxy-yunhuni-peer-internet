a6dd46679d64b546fada9abbe5e9f89b608d976b
liups <liups@yunhuni.com>
2017-03-08 10:12:10 +0800

    YHNI-533 消息API

---
+++ app-msg/src/main/java/com/lsxy/msg/supplier/model/ResultOne.java
@@ -0,0 +1,13 @@
+package com.lsxy.msg.supplier.model;
+
+/**
+ * Created by zhangxb on 2017/1/10.
+ */
+public class ResultOne extends BaseResult {
+    public ResultOne() {
+    }
+
+    public ResultOne(ResultCode result) {
+        super(result);
+    }
+}
