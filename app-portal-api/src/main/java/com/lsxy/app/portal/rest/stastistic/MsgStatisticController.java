4883c12cdca9b8310d3cbb469822f424eaced48f
zhangxb <zhangxb@yunhuni.com>
2017-03-13 19:50:03 +0800

    用户中心-消息统计-前端基本完成

---
+++ app-portal-api/src/main/java/com/lsxy/app/portal/rest/stastistic/MsgStatisticController.java
@@ -0,0 +1,96 @@
+package com.lsxy.app.portal.rest.stastistic;
+
+import com.lsxy.app.portal.base.AbstractRestController;
+import com.lsxy.app.portal.rest.stastistic.vo.MsgStatisticsVo;
+import com.lsxy.framework.core.utils.DateUtils;
+import com.lsxy.framework.core.utils.Page;
+import com.lsxy.framework.web.rest.RestResponse;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by zhangxb on 2017/3/13.
+ */
+@RequestMapping("/rest/msg_statistics")
+@RestController
+public class MsgStatisticController extends AbstractRestController {
+    private static final Logger logger = LoggerFactory.getLogger(MsgStatisticController.class);
+    public static final String TYPE_MONTH = "month";//月统计类型 按年查找输出 返回按年
+    public static final String TYPE_DAY = "day";//日统计类型 按月查找输出 返回按月
+    @RequestMapping("/plist")
+    public RestResponse pList(@RequestParam(defaultValue = "1") Integer pageNo, @RequestParam(defaultValue = "20") Integer pageSize,@RequestParam String tenantId,@RequestParam String appId,@RequestParam String startTime,@RequestParam String type  ){
+        Date date1 = getStartDate(startTime,type);
+        Date date2 = getLastDate(startTime,type);
+        Page page = getPage(tenantId, appId , type, date1, date2, pageNo, pageSize);
+        return RestResponse.success(page);
+    }
+    @RequestMapping("/list")
+    public RestResponse list(@RequestParam String tenantId,@RequestParam String appId,@RequestParam String startTime,@RequestParam String type ){
+        Date date1 = getStartDate(startTime,type);
+        Date date2 = getLastDate(startTime,type);
+        List list = getList( tenantId, appId,date1,date2,type);
+        return RestResponse.success(list);
+    }
+    private Page<MsgStatisticsVo> getPage(String tenantId,String appId ,String type,Date date1,Date date2,int pageNo,int pageSize){
+        List<MsgStatisticsVo> list = getList(tenantId,appId,date1,date2,type);
+        Page page = new Page( (pageNo-1)*pageSize ,  list.size(),  pageSize, list);
+        return page;
+    }
+    private List<MsgStatisticsVo> getList(String tenantId, String appId, Date date1,Date date2,String type){
+        int len = 0;
+        if(TYPE_MONTH.equals(type)){
+            len = getLong(12);
+        }else{
+            len = getLong(date1);
+        }
+        List<MsgStatisticsVo> list = new ArrayList<>();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date1);
+        for (int i = 0; i < len; i++) {
+            list.add( MsgStatisticsVo.initMsgStatisticsVo(calendar.getTime(),(i+1)) );
+            if(TYPE_MONTH.equals(type)){
+                calendar.add(Calendar.MONTH,1);
+            }else{
+                calendar.add(Calendar.DAY_OF_MONTH,1);
+            }
+        }
+        return list;
+    }
+    private Date getStartDate(String startTime,String type){
+        Date date = null;
+        if(TYPE_MONTH.equals(type)){
+            date = DateUtils.parseDate(startTime,"yyyy");
+        }else{
+            date = DateUtils.parseDate(startTime,"yyyy-MM");
+        }
+        return date;
+    }
+    private Date getLastDate(String endTime,String type){
+        Date date2 = null;
+        if(TYPE_MONTH.equals(type)){
+            date2  = DateUtils.parseDate(DateUtils.getLastYearByDate(endTime)+" 23:59:59","yyyy-MM-dd HH:mm:ss");
+        }else{
+            date2 =  DateUtils.parseDate(DateUtils.getMonthLastTime(DateUtils.parseDate(endTime,"yyyy-MM")),"yyyy-MM-dd HH:mm:ss");
+        }
+        return date2;
+    }
+    private int getLong(Object obj){
+        int r = 0;
+        if (obj instanceof Date) {
+            r = Integer.valueOf(DateUtils.getLastDate((Date)obj).split("-")[2]);
+        } else if (obj instanceof Integer) {
+            r =Integer.valueOf((Integer)obj);
+        }
+        return r;
+    }
+
+}
--- app-portal/src/main/java/com/lsxy/app/portal/console/statistics/MsgStatisticsController.java
+++ app-portal/src/main/java/com/lsxy/app/portal/console/statistics/MsgStatisticsController.java
@@ -1,14 +1,22 @@
 package com.lsxy.app.portal.console.statistics;
 
 import com.lsxy.app.portal.base.AbstractPortalController;
+import com.lsxy.app.portal.comm.PortalConstants;
+import com.lsxy.framework.core.utils.DateUtils;
+import com.lsxy.framework.web.rest.RestRequest;
+import com.lsxy.framework.web.rest.RestResponse;
 import com.lsxy.yunhuni.api.app.model.App;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.*;
+
 
 /**
  * Created by zhangxb on 2017/3/10.
@@ -25,5 +33,89 @@ public class MsgStatisticsController extends AbstractPortalController {
         mav.addObject("consumeStatisticsVo",consumeStatisticsVo);//时间
         mav.setViewName("/console/statistics/msg/index");
         return mav;
+    }
+    /**
+     * 异步分页信息
+     * @param request
+     * @param consumeStatisticsVo
+     * @param pageNo
+     * @param pageSize
+     * @return
+     */
+    @RequestMapping("/plist")
+    @ResponseBody
+    public RestResponse pageList(HttpServletRequest request,ConsumeStatisticsVo consumeStatisticsVo,
+                                 @RequestParam(defaultValue = "1") Integer pageNo, @RequestParam(defaultValue = "20")Integer pageSize){
+        String token = getSecurityToken(request);
+        String uri = PortalConstants.REST_PREFIX_URL  + "/rest/msg_statistics/plist?tenantId={1}&appId={2}&type={3}&startTime={4}&pageNo={5}&pageSize={6}";
+        Class clazz = MsgStatisticsVo.class;
+        String appId = "-1".equals(consumeStatisticsVo.getAppId())?null:consumeStatisticsVo.getAppId();
+        String tenantId = getCurrentAccount(request).getTenant().getId();
+        RestResponse restResponse = RestRequest.buildSecurityRequest(token).getPage(uri,clazz ,tenantId,appId,consumeStatisticsVo.getType(),consumeStatisticsVo.getStartTime(),pageNo,pageSize);
+        return restResponse;
+    }
+
+    /**
+     * 异步获取图表显示信息
+     * @param request
+     * @param consumeStatisticsVo
+     * @return
+     */
+    @RequestMapping("/list")
+    @ResponseBody
+    public RestResponse list(HttpServletRequest request,ConsumeStatisticsVo consumeStatisticsVo){
+        String token = getSecurityToken(request);
+        String appId = "-1".equals(consumeStatisticsVo.getAppId())?null:consumeStatisticsVo.getAppId();
+        String tenantId = getCurrentAccount(request).getTenant().getId();
+        String uri = PortalConstants.REST_PREFIX_URL  +   "/rest/msg_statistics/list?tenantId={1}&appId={2}&startTime={3}&type={4}";
+        RestResponse<List<MsgStatisticsVo>> restResponse =  RestRequest.buildSecurityRequest(token).getList(uri, MsgStatisticsVo.class,tenantId,appId,consumeStatisticsVo.getStartTime(),consumeStatisticsVo.getType());
+        List<MsgStatisticsVo> list =  restResponse.getData();
+        Map map = new HashMap();
+        if(ConsumeStatisticsVo.TYPE_MONTH.equals(consumeStatisticsVo.getType())){
+            Map map1 = getArrays(list, 12);
+            map.putAll(map1);
+        }else {
+            Map map1 = getArrays(list, DateUtils.parseDate(consumeStatisticsVo.getStartTime(), "yyyy-MM"));
+            map.putAll(map1);
+        }
+        map.put("count",list.size());
+        return RestResponse.success(map);
+    }
+
+    /**
+     * 获取列表数据
+     * @param list 待处理的list
+     * @return
+     */
+    private Map getArrays(List list,Object date) {
+        int leng = getLong(date);
+        Long[] succ = new Long[leng];
+        Long[] fail = new Long[leng];
+        for(int j=0;j<leng;j++){
+            succ[j] = 0L;
+            fail[j] = 0L;
+        }
+        for(int i=0;i<list.size();i++){
+            Object obj = list.get(i);
+            if(obj instanceof MsgStatisticsVo){
+                MsgStatisticsVo temp = (MsgStatisticsVo)obj;
+                int index = temp.getNum()-1;
+                succ[index] = temp.getTotalSucc();
+                fail[index] = temp.getTotalFail();
+            }
+        }
+        return new HashMap<String,Long[]>(){{
+            put("succ",succ);
+            put("fail",fail);
+        }};
+    }
+    private int getLong(Object obj){
+        int r = 0;
+        if (obj instanceof Date) {
+            r = Integer.valueOf(DateUtils.getLastDate((Date)obj).split("-")[2]);
+        } else if (obj instanceof Integer) {
+            r =Integer.valueOf((Integer)obj);
+        }
+        return r;
     }
 }
