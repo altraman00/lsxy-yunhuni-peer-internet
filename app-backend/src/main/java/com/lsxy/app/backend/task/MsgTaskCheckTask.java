f5d9e02c65fcdd4cbbdc55aa7c0dca06aaa7af64
liups <liups@yunhuni.com>
2017-03-16 17:11:21 +0800

    YHNI-533 消息发送定时检测任务

---
+++ app-backend/src/main/java/com/lsxy/app/backend/task/MsgTaskCheckTask.java
@@ -0,0 +1,38 @@
+package com.lsxy.app.backend.task;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.lsxy.msg.api.service.MsgTaskService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+
+/**
+ * Created by liups on 2017/3/16.
+ */
+public class MsgTaskCheckTask {
+    private static final Logger logger = LoggerFactory.getLogger(MsgTaskCheckTask.class);
+    @Reference(timeout=3000,check = false,lazy = true)
+    MsgTaskService msgTaskService;
+
+    @Scheduled(cron="0 0/12 * * * ?")
+    public void scheduled_massTaskRequest_yyyyMMddHHmm(){
+        msgTaskService.massTaskRequestUpdate();
+    }
+
+    @Scheduled(cron="0 0/10 * * * ?")
+    public void scheduled_paoPaoYuMassTask_yyyyMMddHHmm(){
+        msgTaskService.paoPaoYuMassTaskUpdate();
+    }
+
+    @Scheduled(cron="0 0/1 * * * ?")
+    public void scheduled_paoPaoYuUssdReSend_yyyyMMddHHmm(){
+        msgTaskService.paoPaoYuUssdReSendTask();
+    }
+
+    @Scheduled(cron="0 0/10 * * * ?")
+    public void scheduled_qiXunTongMassTask_yyyyMMddHHmm(){
+        msgTaskService.qiXunTongMassTaskUpdate();
+    }
+
+
+}
