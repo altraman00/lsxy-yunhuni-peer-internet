312c1c332012cdbeffb63a7774bae2897a58503d
liups <liups@yunhuni.com>
2017-03-20 16:30:08 +0800

    YHNI-561 录音文件下载准备完毕异步通知

--- app-area-server/src/main/java/com/lsxy/area/server/mq/handler/RecordFileDownloadNotificationHandler.java
+++ app-area-server/src/main/java/com/lsxy/area/server/mq/handler/RecordFileDownloadNotificationHandler.java
@@ -1,9 +1,15 @@
 package com.lsxy.area.server.mq.handler;
 
+import com.lsxy.area.server.util.NotifyCallbackUtil;
+import com.lsxy.framework.core.utils.MapBuilder;
 import com.lsxy.framework.core.utils.OssTempUriUtils;
 import com.lsxy.framework.mq.api.MQMessageHandler;
 import com.lsxy.framework.mq.api.MQService;
 import com.lsxy.framework.mq.events.apigw.RecordFileDownloadNotificationEvent;
+import com.lsxy.yunhuni.api.apicertificate.model.ApiCertificateSubAccount;
+import com.lsxy.yunhuni.api.apicertificate.service.ApiCertificateSubAccountService;
+import com.lsxy.yunhuni.api.app.model.App;
+import com.lsxy.yunhuni.api.app.service.AppService;
 import com.lsxy.yunhuni.api.file.model.VoiceFileRecord;
 import com.lsxy.yunhuni.api.file.service.VoiceFileRecordService;
 import org.apache.commons.lang.StringUtils;
@@ -12,6 +18,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.jms.JMSException;
+import java.util.Map;
 
 /**
  * Created by liups on 2017/3/20.
@@ -22,6 +29,13 @@ public class RecordFileDownloadNotificationHandler implements MQMessageHandler<R
     VoiceFileRecordService voiceFileRecordService;
     @Autowired
     MQService mqService;
+    @Autowired
+    private NotifyCallbackUtil notifyCallbackUtil;
+    @Autowired
+    private ApiCertificateSubAccountService apiCertificateSubAccountService;
+    @Autowired
+    private AppService appService;
+
     @Override
     public void handleMessage(RecordFileDownloadNotificationEvent message) throws JMSException {
         if(logger.isDebugEnabled()){
@@ -41,8 +55,34 @@ public class RecordFileDownloadNotificationHandler implements MQMessageHandler<R
             if(logger.isDebugEnabled()) {
                 logger.debug("生成ossUri地址：[{}]", ossUri);
             }
-            //TODO 发送通知
-
+            String callbackUrl = null;
+            String subaccountId = message.getSubaccountId();
+            if(StringUtils.isNotBlank(subaccountId)){
+                ApiCertificateSubAccount subAccount = apiCertificateSubAccountService.findById(subaccountId);
+                String subCallbackUrl = subAccount.getCallbackUrl();
+                if(StringUtils.isNotBlank(subCallbackUrl)){
+                    callbackUrl = subCallbackUrl;
+                }
+            }
+            if(StringUtils.isNotBlank(callbackUrl)){
+                App app = appService.findById(message.getAppId());
+                String url = app.getUrl();
+                if(StringUtils.isNotBlank(url)){
+                    callbackUrl = url;
+                }
+            }
+            if(StringUtils.isBlank(callbackUrl)){
+                logger.error("缺少回调地址");
+                return;
+            }
+            Map<String,Object> notify_data = new MapBuilder<String,Object>()
+                    .putIfNotEmpty("event","recording.download_preparing")
+                    .putIfNotEmpty("id",message.getRecordFileId())
+                    .putIfNotEmpty("subaccount_id",message.getSubaccountId())
+                    .putIfNotEmpty("url",ossUri)
+                    .build();
+            // 发送通知
+            notifyCallbackUtil.postNotify(callbackUrl,notify_data,null,3);
         }else{
             mqService.publish(new RecordFileDownloadNotificationEvent(message.getAppId(),message.getSubaccountId(),voiceFileRecord.getId(),message.getCount() + 1));
         }
