9d1da6f820a524920a52a8af3268b6e97019aef8
zhangxb <zhangxb@yunhuni.com>
2017-03-24 18:24:47 +0800

    修改显示

---
+++ module-framework-third/src/main/java/com/msg/jimu/JiMuClient.java
@@ -0,0 +1,10 @@
+package com.msg.jimu;
+
+/**
+ * 积木通讯
+ * Created by zhangxb on 2017/3/24.
+ */
+public class JiMuClient {
+    private static final String AppId = "b305886d5a674ea19cb89b540848c2fd";
+    private static final String App_SECRET = "";
+}
